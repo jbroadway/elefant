275000cef9a8bbc41f3c60d128a00b27419efa69
jbroadway <johnny@johnnybroadway.com>
2011-04-29 12:32:03 -0500

    Added layout template for global rendering

--- lib/Page.php
+++ lib/Page.php
@@ -7,7 +7,12 @@
  *
  * The template property sets which template should be rendered
  * for the current request. The default is 'base' which renders
- * views/base.html. To skip a template altogether, simply use:
+ * views/base.html, which is then passed to views/layout.html
+ * to render the overall layout, unless you specify:
+ *
+ *   $page->layout = false;
+ *
+ * To skip a template altogether for things like JSON, use:
  *
  *   $page->template = false;
  *
@@ -19,12 +24,17 @@ class Page {
 	var $title = '';
 	var $body = '';
 	var $template = 'base';
+	var $layout = 'layout';
 
 	function render () {
-		if ($this->template == false) {
+		if ($this->template === false) {
 			return $this->body;
 		}
 		global $tpl;
+		if ($this->layout) {
+			$this->body = $tpl->render ($this->template, $this);
+			return $tpl->render ($this->layout, $this);
+		}
 		return $tpl->render ($this->template, $this);
 	}
 }
--- views/cache/base.php
+++ views/cache/base.php
@@ -1,10 +1,2 @@
-<html>
-<head>
-<title><?php echo htmlspecialchars ($data->title, ENT_QUOTES, 'UTF-8'); ?></title>
-<?php echo $data->head; ?>
-</head>
-<body>
 <h1><?php echo htmlspecialchars ($data->title, ENT_QUOTES, 'UTF-8'); ?></h1>
 <?php echo $data->body; ?>
-</body>
-</html>
