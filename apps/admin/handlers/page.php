cd75eafd741b5a407bce11bfbc867552d7150821
Johnny Broadway <johnny@johnnybroadway.com>
2011-05-05 10:45:56 -0500

    Added 404 header to default handler

--- apps/admin/handlers/page.php
+++ apps/admin/handlers/page.php
@@ -5,6 +5,7 @@ $id = count ($this->params) ? $this->params[0] : 'index';
 $wp = new Webpage ($id);
 
 if ($wp->error) {
+	header ('HTTP/1.1 404 Not Found');
 	$page->title = 'Page Not Found';
 	$page->template = 'admin/base';
 	$page->layout = 'error';
