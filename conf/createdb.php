d881e35f0c40270e397f6934b30fdbd8c96f0607
jbroadway <johnny@johnnybroadway.com>
2011-05-09 21:17:35 -0500

    Updated createdb for latest database lib changes

--- conf/createdb.php
+++ conf/createdb.php
@@ -6,7 +6,9 @@ if (basename (getcwd ()) == 'conf') {
 require_once ('lib/Functions.php');
 require_once ('lib/Database.php');
 
-$db = db_open (array ('driver' => 'sqlite', 'file' => 'conf/site.db'));
+if (! db_open (array ('driver' => 'sqlite', 'file' => 'conf/site.db'))) {
+	die (db_error ());
+}
 foreach (sql_split (file_get_contents ('conf/install_sqlite.sql')) as $sql) {
 	if (! db_execute ($sql)) {
 		echo 'Error: ' . db_error () . "\n";
