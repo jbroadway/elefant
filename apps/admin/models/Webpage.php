69c1cae73d04566e2066e445f369fd4acd0b788b
jbroadway <johnny@johnnybroadway.com>
2011-06-05 21:19:15 -0500

    Updated Webpage class for new fields

--- apps/admin/models/Webpage.php
+++ apps/admin/models/Webpage.php
@@ -5,10 +5,29 @@
  *
  * id
  * title
- * head (for additional script/style/meta includes)
+ * menu_title
+ * window_title
+ * weight
+ * head (virtual, for description/keyword inclusion)
  * layout
+ * description
+ * keywords
  * body
  */
-class Webpage extends Model {}
+class Webpage extends Model {
+	function __get ($key) {
+		if ($key == 'head') {
+			$head = '';
+			if (isset ($this->data['description'])) {
+				$head .= '<meta name="description" content="' . Template::sanitize ($this->data['description']) . "\" />\n";
+			}
+			if (isset ($this->data['keywords'])) {
+				$head .= '<meta name="keywords" content="' . Template::sanitize ($this->data['keywords']) . "\" />\n";
+			}
+			return $head;
+		}
+		return parent::__get ($key);
+	}
+}
 
 ?>
