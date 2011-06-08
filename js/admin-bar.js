f6565c12b4d044ea382f91d6811b493ce28df6c6
jbroadway <johnny@johnnybroadway.com>
2011-06-07 19:07:57 -0500

    Admin title links to home

--- js/admin-bar.js
+++ js/admin-bar.js
@@ -1,5 +1,5 @@
 $(function () {
-	$('body').append ('<div id="admin-bar"><div id="admin-links"></div><span><em>Elefant</em>Admin</span></div>');
+	$('body').append ('<div id="admin-bar"><div id="admin-links"></div><a href="/"><span><em>Elefant</em>Admin</span></a></div>');
 	$.get ('/admin/head/links', function (res) {
 		$('#admin-links').append (res);
 		$('#admin-tools').hover (function () {
