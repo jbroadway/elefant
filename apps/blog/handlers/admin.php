035df7576e267eba9220183ffa0730540fabd8b4
jbroadway <johnny@johnnybroadway.com>
2011-06-22 23:39:39 -0500

    Added initial admin screen for blog posts

---
+++ apps/blog/handlers/admin.php
@@ -0,0 +1,30 @@
+<?php
+
+$page->layout = 'admin';
+
+if (! User::require_admin ()) {
+	header ('Location: /admin');
+	exit;
+}
+
+require_once ('apps/blog/lib/Filters.php');
+
+$limit = 20;
+$_GET['offset'] = (isset ($_GET['offset'])) ? $_GET['offset'] : 0;
+
+$posts = blog\Post::query ('id, title, ts, author')
+	->order ('ts desc')
+	->fetch_orig ($limit, $_GET['offset']);
+$count = blog\Post::query ()->count ();
+
+$page->title = i18n_get ('Blog Posts');
+echo $tpl->render ('blog/admin', array (
+	'posts' => $posts,
+	'count' => $count,
+	'offset' => $_GET['offset'],
+	'more' => ($count > $_GET['offset'] + $limit) ? true : false,
+	'prev' => $_GET['offset'] - $limit,
+	'next' => $_GET['offset'] + $limit
+));
+
+?>
