9d282fd1649cdb45bfcf33dcd943672ad03bd130
Johnny Broadway <johnny@johnnybroadway.com>
2011-06-27 15:24:52 -0500

    Sidebar on default is now calling a block

--- apps/blocks/handlers/index.php
+++ apps/blocks/handlers/index.php
@@ -1,21 +1,25 @@
 <?php
 
-$id = (isset ($data['id'])) ? $data['id'] : (isset ($_GET['id'])) ? $_GET['id'] : false;
+$id = (isset ($data['id'])) ? $data['id'] : ((isset ($_GET['id'])) ? $_GET['id'] : false);
 if (! $id) {
 	// no id
+	echo 'no id';
 	return;
 }
 
 $b = new Block ($id);
 if ($b->error) {
 	// not found
+	echo 'not found';
 	return;
 }
 
 // permissions
 if ($b->access == 'member' && ! User::require_login ()) {
+	echo 'member only';
 	return;
 } elseif ($wp->access == 'private' && ! User::require_admin ()) {
+	echo 'private';
 	return;
 }
 
