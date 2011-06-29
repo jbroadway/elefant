b7e09cd16d6265b7cd4f1a22c2ef26f4295e3dd4
jbroadway <johnny@johnnybroadway.com>
2011-06-29 18:33:46 -0500

    Page embeds now render in previews

--- apps/admin/handlers/preview.php
+++ apps/admin/handlers/preview.php
@@ -2,16 +2,16 @@
 
 $wp = new Webpage ($_POST);
 
-$page->id = $wp->id;
-$page->title = $wp->title;
-$page->menu_title = (! empty ($wp->menu_title)) ? $wp->menu_title : $wp->title;
-$page->window_title = (! empty ($wp->window_title)) ? $wp->window_title : $wp->title;
-$page->description = $wp->description;
-$page->keywords = $wp->keywords;
+$page->id = $_POST['id'];
+$page->title = $_POST['title'];
+$page->menu_title = (! empty ($_POST['menu_title'])) ? $_POST['menu_title'] : $_POST['title'];
+$page->window_title = (! empty ($_POST['window_title'])) ? $_POST['window_title'] : $_POST['title'];
+$page->description = $_POST['description'];
+$page->keywords = $_POST['keywords'];
 $page->template = 'admin/base';
-$page->layout = $wp->layout;
-$page->head = $wp->head;
+$page->layout = $_POST['layout'];
+$page->head = '';
 
-echo $wp->body;
+echo $tpl->run_includes ($_POST['body']);
 
 ?>
