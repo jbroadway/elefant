343163172cbe06a1821982de4a05e3f808a0837c
Johnny Broadway <johnny@johnnybroadway.com>
2011-07-15 13:18:01 -0500

    Added placeholder text to admin/forward so admins can edit those pages

--- apps/admin/handlers/forward.php
+++ apps/admin/handlers/forward.php
@@ -1,5 +1,17 @@
 <?php
 
+global $user;
+if (User::is_valid () && $user->type == 'admin') {
+	$to = isset ($data['to']) ? $data['to'] : $_GET['to'];
+	printf (
+		'<p>%s:</p><p><a href="%s">%s</a></p>',
+		i18n_get ('This page forwards visitors to the following link'),
+		$to,
+		$to
+	);
+	return;
+}
+
 if (isset ($data['to'])) {
 	header ('Location: ' . $data['to']);
 } elseif (isset ($_GET['to'])) {
