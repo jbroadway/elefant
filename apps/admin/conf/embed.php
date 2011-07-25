2a1271347562c1553dae394a67e511daab7f010a
Johnny Broadway <johnny@johnnybroadway.com>
2011-07-25 09:53:10 -0500

    Added input validation on link field for redirects

--- apps/admin/conf/embed.php
+++ apps/admin/conf/embed.php
@@ -7,6 +7,9 @@ label = "Pages: Redirect Link"
 to[label] = Link
 to[type] = text
 to[initial] = "http://"
+to[not empty] = 1
+to[regex] = "|^http://.+$|"
+to[message] = Please enter a valid URL.
 
 [admin/menu]
 
