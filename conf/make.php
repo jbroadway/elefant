f981041d7fa6cfafc14c51be5c7589130d7abc81
Johnny Broadway <johnny@johnnybroadway.com>
2011-07-26 09:43:22 -0500

    Improved the conf/make.php app scaffolding

--- conf/make.php
+++ conf/make.php
@@ -19,8 +19,21 @@ mkdir ('apps/' . $argv[1] . '/lib', 0755, true);
 mkdir ('apps/' . $argv[1] . '/models', 0755, true);
 mkdir ('apps/' . $argv[1] . '/views', 0755, true);
 
-file_put_contents ('apps/' . $argv[1] . '/handlers/index.php', '<?php $page->template = \'' . $argv[1] . '/index\'; ?>');
+file_put_contents ('apps/' . $argv[1] . '/handlers/index.php', sprintf (
+	"<?php\n\n\$page->title = '%s home';\n\$page->template = '%s/index';\n\n?>",
+	ucfirst ($argv[1]),
+	$argv[1]
+));
+file_put_contents ('apps/' . $argv[1] . '/handlers/admin.php', sprintf (
+	"<?php\n\nif (! User::require_admin ()) {\n\t\$this->redirect ('/admin');\n}\n\n\$page->title = '%s admin';\n\$page->layout = 'admin';\n\n?>",
+	ucfirst ($argv[1])
+));
 file_put_contents ('apps/' . $argv[1] . '/views/index.html', '<p>{{ body|none }}</p>');
+file_put_contents ('apps/' . $argv[1] . '/conf/config.php', sprintf (
+	"; <?php\n\n[Admin]\n\nhandler = %s/admin\nname = %s\n\n; */ ?>",
+	$argv[1],
+	ucfirst ($argv[1])
+));
 
 echo "Done.\n";
 
