ceedfd290eaac740bdea9a05e5bb34fe0bb1108a
jbroadway <johnny@johnnybroadway.com>
2011-08-07 18:55:15 -0500

    Updated autosave code in all forms

--- apps/admin/handlers/cancel.php
+++ apps/admin/handlers/cancel.php
@@ -8,14 +8,6 @@ if (! User::require_admin ()) {
 $lock = new Lock ($_GET['type'], $_GET['id']);
 $lock->remove ();
 
-// clear autosave
-$parts = explode ('/', $_SERVER['HTTP_REFERER']);
-array_shift ($parts);
-array_shift ($parts);
-array_shift ($parts);
-$cookie_name = 'autosave-' . preg_replace ('/[^a-z0-9-]/', '', join ('', $parts));
-setcookie ($cookie_name, '', time () - 900, '/');
-
 header ('Location: ' . $_GET['return']);
 exit;
 
