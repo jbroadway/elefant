516b29650bbd7a0ed311a035ca7f487645d7a9a5
jbroadway <johnny@johnnybroadway.com>
2011-08-07 17:41:40 -0500

    Restore now clears autosave data, added lzw compression to reduce cookie sizes

--- js/jquery.autosave.js
+++ js/jquery.autosave.js
@@ -73,7 +73,7 @@ var autosave_interval = null,
 			$('.autosave-restore').click (function () {
 				var i = 0,
 					opts = options,
-					vals = $.parseJSON ($.cookie (opts.cookie_name));
+					vals = $.parseJSON (lzw_decode ($.cookie (opts.cookie_name)));
 
 				for (i = 0; i < vals.length; i++) {
 					try {
@@ -104,6 +104,9 @@ var autosave_interval = null,
 						}
 					} catch (e) {}
 				}
+
+				// Clear the cookie
+				$.cookie (opts.cookie_name, null, {path: '/'});
 
 				// Hide the restore notice
 				$('.autosave-notice').slideUp ('slow');
@@ -164,8 +167,64 @@ var autosave_interval = null,
 					}
 				}
 				
-				$.cookie (opts.cookie_name, JSON.stringify (vals), { expires: 1, path: '/' });
+				var _json = JSON.stringify (vals);
+				$.cookie (opts.cookie_name, lzw_encode (JSON.stringify (vals)), { expires: 1, path: '/' });
 			}, options.interval);
 		}
 	});
 })(jQuery);
+
+// The following are from http://jsolait.net/
+
+// LZW-compress a string
+function lzw_encode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var out = [];
+    var currChar;
+    var phrase = data[0];
+    var code = 256;
+    for (var i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
+    }
+    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+    for (var i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
+}
+
+// Decompress an LZW-encoded string
+function lzw_decode(s) {
+    var dict = {};
+    var data = (s + "").split("");
+    var currChar = data[0];
+    var oldPhrase = currChar;
+    var out = [currChar];
+    var code = 256;
+    var phrase;
+    for (var i=1; i<data.length; i++) {
+        var currCode = data[i].charCodeAt(0);
+        if (currCode < 256) {
+            phrase = data[i];
+        }
+        else {
+           phrase = dict[currCode] ? dict[currCode] : (oldPhrase + currChar);
+        }
+        out.push(phrase);
+        currChar = phrase.charAt(0);
+        dict[code] = oldPhrase + currChar;
+        code++;
+        oldPhrase = phrase;
+    }
+    return out.join("");
+}
