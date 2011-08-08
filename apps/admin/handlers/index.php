cf0051742b333a080d2dc6c364f57c373e5d0332
Johnny Broadway <johnny@johnnybroadway.com>
2011-08-08 11:09:56 -0500

    Improved admin login screen and default layouts

--- apps/admin/handlers/index.php
+++ apps/admin/handlers/index.php
@@ -3,7 +3,7 @@
 $page->layout = 'admin';
 
 if (! User::require_admin ()) {
-	$page->title = 'Elefant Admin';
+	$page->title = '<img src="/apps/admin/css/admin/elefant_logo_login.png" alt="Elefant CMS" style="margin-left: -7px" />';
 	if (! empty ($_POST['username'])) {
 		echo '<p>' . i18n_get ('Incorrect email or password, please try again.') . '</p>';
 	} else {
