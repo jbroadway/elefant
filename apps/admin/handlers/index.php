069543af824c2f322e80844cd97aa9a76a9017f4
Johnny Broadway <johnny@johnnybroadway.com>
2011-08-08 11:16:55 -0500

    Fixed window_title on admin login

--- apps/admin/handlers/index.php
+++ apps/admin/handlers/index.php
@@ -4,6 +4,7 @@ $page->layout = 'admin';
 
 if (! User::require_admin ()) {
 	$page->title = '<img src="/apps/admin/css/admin/elefant_logo_login.png" alt="Elefant CMS" style="margin-left: -7px" />';
+	$page->window_title = i18n_get ('Please log in to continue.');
 	if (! empty ($_POST['username'])) {
 		echo '<p>' . i18n_get ('Incorrect email or password, please try again.') . '</p>';
 	} else {
