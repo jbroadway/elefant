0a7ccf1c5328fe4520483e8dcea2909393b21f67
Johnny Broadway <johnny@johnnybroadway.com>
2011-08-22 09:13:49 -0500

    404 error handler forwards to installer if it hasn't been run yet

--- apps/admin/handlers/error.php
+++ apps/admin/handlers/error.php
@@ -1,5 +1,9 @@
 <?php
 
+if ($this->data['code'] == 404 && @file_exists ('install') && ! @file_exists ('install/installed')) {
+	$this->redirect ('/install');
+}
+
 header ('HTTP/1.1 ' . $this->data['code'] . ' ' . $this->data['title']);
 
 $page->title = $this->data['title'];
