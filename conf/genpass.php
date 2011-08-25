47006b9250617799a87bf72c5693865d3ad45c1c
Johnny Broadway <johnny@johnnybroadway.com>
2011-08-25 11:27:04 -0500

    Added conf/genpass.php to generate encrypted passwords for sites not using the installers

---
+++ conf/genpass.php
@@ -0,0 +1,22 @@
+<?php
+
+if (php_sapi_name () !== 'cli') {
+	die ("For use on the command line only.\n");
+}
+
+if ($argc == 1) {
+	die ("Usage: php conf/genpass.php password\n");
+}
+
+function encrypt_pass ($plain) {
+	$base = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	$salt = '$1$';
+	for ($i = 0; $i < 9; $i++) {
+		$salt .= $base[rand (0, 61)];
+	}
+	return crypt ($plain, $salt . '$');
+}
+
+echo encrypt_pass ($argv[1]) . "\n";
+
+?>
