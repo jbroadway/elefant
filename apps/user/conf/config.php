15b110ddcbe8dc4a16609565f8d5a0454f55ef02
jbroadway <johnny@johnnybroadway.com>
2011-09-07 23:12:22 -0500

    Added login_methods and Facebook and Twitter id/secret values to user app conf

--- apps/user/conf/config.php
+++ apps/user/conf/config.php
@@ -2,8 +2,40 @@
 
 [User]
 
+; This is the page you want to redirect users to after
+; they log out.
+
 logout_redirect = "/"
+
+; This is a list of user types available on your site.
+
 user_types = "admin, member"
+
+; You can enable or disable alternative login methods here.
+; Each name corresponds to a handler in apps/user/handlers/login.
+
+login_methods[] = openid
+login_methods[] = google
+;login_methods[] = twitter
+;login_methods[] = facebook
+
+[Facebook]
+
+; To enable Facebook login support, register your site at
+; https://developers.facebook.com/apps to generate the following
+; values for your site.
+
+application_id = ""
+application_secret = ""
+
+[Twitter]
+
+; To enable Twitter login support, register your site at
+; https://dev.twitter.com/apps/new to generate the following
+; values for your site.
+
+consumer_key = ""
+consumer_secret = ""
 
 [Admin]
 
