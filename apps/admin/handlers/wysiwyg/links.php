a91d3f00b8dded1933e2f8540498f61253d8cac8
Johnny Broadway <johnny@johnnybroadway.com>
2011-10-11 08:39:54 -0500

    Fixed link list in wysiwyg link dialogs

--- apps/admin/handlers/wysiwyg/links.php
+++ apps/admin/handlers/wysiwyg/links.php
@@ -1,17 +1,30 @@
 <?php
 
+/**
+ * Returns a list of internal page links to the WYSIWYG editor's
+ * link dialog.
+ *
+ * Note: Only includes public pages.
+ */
+
+function admin_links_sort ($a, $b) {
+	if ($a['title'] == $b['title']) {
+		return 0;
+	}
+	return ($a['title'] < $b['title']) ? -1 : 1;
+}
+
 $page->layout = false;
 
 $menu = Webpage::query ('id, title, menu_title')
 		->where ('access', 'public')
-		->where ('weight > -1') // negative weight leaves pages out of menus
-		->order ('weight desc')
 		->fetch_orig ();
 $out = array ();
 foreach ($menu as $pg) {
 	$mt = (! empty ($pg->menu_title)) ? $pg->menu_title : $pg->title;
 	$out[] = array ('url' => '/' . $pg->id, 'title' => $mt);
 }
+uasort ($out, 'admin_links_sort');
 
 echo json_encode ($out);
 
