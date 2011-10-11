1ce2b2c1e61f26fb750f2273eb66ce8d0205f88e
Johnny Broadway <johnny@johnnybroadway.com>
2011-10-11 08:39:42 -0500

    Fixed link list in wysiwyg link dialogs

--- apps/admin/handlers/wysiwyg/links.php
+++ apps/admin/handlers/wysiwyg/links.php
@@ -1,17 +1,23 @@
 <?php
 
+function admin_links_sort ($a, $b) {
+	if ($a['title'] == $b['title']) {
+		return 0;
+	}
+	return ($a['title'] < $b['title']) ? -1 : 1;
+}
+
 $page->layout = false;
 
 $menu = Webpage::query ('id, title, menu_title')
 		->where ('access', 'public')
-		->where ('weight > -1') // negative weight leaves pages out of menus
-		->order ('weight desc')
 		->fetch_orig ();
 $out = array ();
 foreach ($menu as $pg) {
 	$mt = (! empty ($pg->menu_title)) ? $pg->menu_title : $pg->title;
 	$out[] = array ('url' => '/' . $pg->id, 'title' => $mt);
 }
+uasort ($out, 'admin_links_sort');
 
 echo json_encode ($out);
 
