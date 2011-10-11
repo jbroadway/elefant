a15eab7acd15a7612295a0bbc936fe460d7b33ef
Johnny Broadway <johnny@johnnybroadway.com>
2011-10-11 08:45:28 -0500

    Added comments to api handlers

--- apps/api/handlers/index.php
+++ apps/api/handlers/index.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Default handler, simply forwards to the current version of the API.
+ */
+
 $page->layout = false;
 
 header ('Location: /api/' . $appconf['Api']['current_version']);
