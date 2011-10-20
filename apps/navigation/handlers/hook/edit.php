e4cdbbc8aa841ee2693688f0ef6d97d9cef420f3
Johnny Broadway <johnny@johnnybroadway.com>
2011-10-19 23:33:55 -0500

    Fixed a bug where navigation wouldn't update its ID value if page ID had changed

--- apps/navigation/handlers/hook/edit.php
+++ apps/navigation/handlers/hook/edit.php
@@ -8,6 +8,10 @@ $n = new Navigation;
 $node = $n->node ($this->data['page']);
 if ($node) {
 	$node->data = (! empty ($this->data['menu_title'])) ? $this->data['menu_title'] : $this->data['title'];
+	if ($this->data['page'] != $this->data['id']) {
+		// Update ID if renamed
+		$node->attr->id = $this->data['id'];
+	}
 	$n->save ();
 }
 
