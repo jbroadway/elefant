30056662ac1aa3e39c3a4fae1ea1c55749a388a5
jbroadway <johnny@johnnybroadway.com>
2011-11-29 14:42:48 -0600

    Switched to using controller's redirect()

--- apps/api/handlers/index.php
+++ apps/api/handlers/index.php
@@ -4,9 +4,6 @@
  * Default handler, simply forwards to the current version of the API.
  */
 
-$page->layout = false;
-
-header ('Location: /api/' . $appconf['Api']['current_version']);
-exit;
+$this->redirect ('/api/' . $appconf['Api']['current_version']);
 
 ?>
