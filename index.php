183349200cb40da5051c9995153fc530b1b2c438
Johnny Broadway <johnny@johnnybroadway.com>
2011-12-01 13:46:17 -0600

    Elefant now dies on db connection failure only if all failed or master failed.

--- index.php
+++ index.php
@@ -24,11 +24,26 @@
  * THE SOFTWARE.
  */
 
-// check the error log for errors
+/**
+ * This is the front controller of Elefant. All dynamic requests
+ * are sent here to be handled and served.
+ */
+
+/**
+ * Set the default error reporting level to All except Notices,
+ * and turn off displaying errors. Error handling/debugging can
+ * be done by setting conf[General][debug] to true, causing full
+ * debug traces to be displayed with highlighted code in the
+ * browser (*for development purposes only*), or by checking
+ * the error log for errors.
+ */
 error_reporting (E_ALL & ~E_NOTICE);
 ini_set ('display_errors', 'Off');
 
-// apparently we still have to deal with this... *sigh*
+/**
+ * Normalize slashes for servers that are still poorly
+ * configured...
+ */
 if (get_magic_quotes_gpc ()) {
 	function stripslashes_gpc (&$value) {
 		$value = stripslashes ($value);
@@ -39,15 +54,27 @@ if (get_magic_quotes_gpc ()) {
 	array_walk_recursive ($_REQUEST, 'stripslashes_gpc');
 }
 
-// get the global configuration
+/**
+ * Check ELEFANT_ENV environment variable to determine which
+ * configuration to load, and load it. Also set the default
+ * timezone to avoid warnings in date functions.
+ */
 define ('ELEFANT_ENV', getenv ('ELEFANT_ENV') ? getenv ('ELEFANT_ENV') : 'config');
 $conf = parse_ini_file ('conf/' . ELEFANT_ENV . '.php', true);
 date_default_timezone_set($conf['General']['timezone']);
+
+/**
+ * Enable the debugger if conf[General][debug] is true.
+ */
 if ($conf['General']['debug']) {
 	require_once ('lib/Debugger.php');
 	Debugger::start ();
 }
 
+/**
+ * Include the core libraries used by the front controller
+ * to dispatch and respond to requests.
+ */
 require_once ('conf/version.php');
 require_once ('lib/Autoloader.php');
 require_once ('lib/Functions.php');
@@ -57,18 +84,31 @@ require_once ('lib/I18n.php');
 require_once ('lib/Controller.php');
 require_once ('lib/Template.php');
 
-// cli support
+/**
+ * If we're on the command line, set the request to use
+ * the first argument passed to the script.
+ */
 if (defined ('STDIN')) {
 	$_SERVER['REQUEST_URI'] = '/' . $argv[1];
 }
 
-// create core objects
+/**
+ * Initialize some core objects. These function as singletons
+ * because only one instance of them per request is desired
+ * (no duplicate execution for things like loading translation
+ * files).
+ */
 $i18n = new I18n ('lang', $conf['I18n']);
 $page = new Page;
 $controller = new Controller ($conf['Hooks']);
 $tpl = new Template ($conf['General']['charset'], $controller);
 
-// initialize cache
+/**
+ * Initialize the built-in Memcache support, or provide a
+ * transparent fallback so we can include caching in our
+ * handlers and in the front controller, whether Memcache
+ * is available or not.
+ */
 if (isset ($conf['Memcache']['server']) && extension_loaded ('memcache')) {
 	$memcache = new MemcacheExt;
 	foreach ($conf['Memcache']['server'] as $s) {
@@ -79,17 +119,33 @@ if (isset ($conf['Memcache']['server']) && extension_loaded ('memcache')) {
 	$memcache = new Cache ();
 }
 
-// connect to the database
+/**
+ * Connect to the databases. Will die if the master connect
+ * fails, or if all connections fail, but will continue
+ * as long as the master connection succeeds since that is
+ * require to issue write commands.
+ */
 foreach (array_keys ($conf['Database']) as $key) {
 	if ($key == 'master') {
 		$conf['Database'][$key]['master'] = true;
 	}
 	if (! db_open ($conf['Database'][$key])) {
-		die (db_error ());
+		if ($conf['Database'][$key]['master'] === true) {
+			// Die immediately if connection to master fails,
+			// since we can't issue any write commands.
+			die (db_error ());
+		}
 	}
 }
+// Die if no connections succeeded.
+if (db_conn_count () === 0) {
+	die (db_error ());
+}
 
-// handle the request
+/**
+ * Route the request to the appropriate handler and get
+ * the handler's response.
+ */
 if ($i18n->url_includes_lang) {
 	$handler = $controller->route ($i18n->new_request_uri);
 } else {
@@ -97,7 +153,10 @@ if ($i18n->url_includes_lang) {
 }
 $page->body = $controller->handle ($handler, false);
 
-// render and send the output
+/**
+ * Render and send the output to the client, using gzip
+ * compression if conf[General][compress_output] is true.
+ */
 $out = $page->render ($tpl);
 if ($conf['General']['compress_output'] && extension_loaded ('zlib')) {
 	ob_start ('ob_gzhandler');
