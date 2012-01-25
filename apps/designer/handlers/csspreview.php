481cf2774e26028f0369ab45248b990046c4f574
jbroadway <johnny@johnnybroadway.com>
2012-01-24 19:08:55 -0600

    Added fancybox preview to designer app layouts/stylesheet direct links

--- apps/designer/handlers/csspreview.php
+++ apps/designer/handlers/csspreview.php
@@ -4,6 +4,8 @@
  * Live preview handler for stylesheet forms.
  */
 
+$this->require_admin ();
+
 $page->title = i18n_get ('Page title');
 $page->preview = true;
 
@@ -13,6 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 	} else {
 		$page->layout = file_get_contents ('layouts/default.html');
 	}
+	$page->layout = str_replace ('</head>', '<style>' . file_get_contents ($_GET['css']) . '</style></head>', $page->layout);
 } else {
 	$page->layout = file_get_contents ('layouts/' . $_POST['layout'] . '.html');
 	$page->layout = str_replace ('</head>', '<style>' . $_POST['css'] . '</style></head>', $page->layout);
