02977a8f28dadaf52ed23ef2008ed1b09c9a39ab
Johnny Broadway <johnny@johnnybroadway.com>
2012-02-07 12:30:46 -0600

    Updated handler outline

--- apps/translator/handlers/index.php
+++ apps/translator/handlers/index.php
@@ -1,10 +1,11 @@
 <?php
 
-if (! User::require_admin ()) {
-	$this->redirect ('/admin');
-}
+$this->require_admin ();
 
-$page->title = 'Translator';
 $page->layout = 'admin';
+
+$page->title = i18n_get ('Translator');
+
+echo $tpl->render ('translator/index');
 
 ?>
