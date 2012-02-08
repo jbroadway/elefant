46013114a674da01c04bf9e877a8052617f8c41f
jbroadway <johnny@johnnybroadway.com>
2012-02-07 22:16:22 -0600

    Added add language form, ini writing to lang/languages.php

---
+++ apps/translator/handlers/add.php
@@ -0,0 +1,43 @@
+<?php
+
+$this->require_admin ();
+
+$page->layout = 'admin';
+
+$page->title = i18n_get ('Add language');
+
+$form = new Form ('post', $this);
+
+require_once ('apps/translator/lib/Functions.php');
+
+echo $form->handle (function ($form) {
+	// Add to lang/languages.php
+
+	if (! empty ($_POST['locale'])) {
+		$lang = $_POST['code'] . '_' . $_POST['locale'];
+	} else {
+		$lang = $_POST['code'];
+	}
+
+	global $i18n;
+
+	$i18n->languages[$lang] = array (
+		'name' => $_POST['name'],
+		'code' => $_POST['code'],
+		'locale' => $_POST['locale'],
+		'charset' => $_POST['charset'],
+		'fallback' => $_POST['fallback'],
+		'default' => 'Off'
+	);
+
+	uasort ($i18n->languages, 'translator_sort_languages');
+
+	if (! file_put_contents ('lang/languages.php', translator_ini_write ($i18n->languages))) {
+		return false;
+	}
+
+	$form->controller->add_notification (i18n_get ('Language added.'));
+	$form->controller->redirect ('/translator/index');
+});
+
+?>
---
+++ apps/translator/lib/Functions.php
@@ -0,0 +1,58 @@
+<?php
+
+function translator_lang_exists ($lang) {
+	global $i18n;
+	
+	if (! empty ($_POST['locale'])) {
+		$lang = $_POST['code'] . '_' . $_POST['locale'];
+	} else {
+		$lang = $_POST['code'];
+	}
+	
+	if (isset ($i18n->languages[$lang])) {
+		return false;
+	}
+	return true;
+}
+
+function translator_sort_languages ($a, $b) {
+	if ($a['name'] === $b['name']) {
+		return 0;
+	}
+	return ($a['name'] < $b['name']) ? -1 : 1;
+}
+
+function translator_ini_write ($data) {
+	$out = "; <?php /*\n\n";
+
+	$write_value = function ($value) {
+		if (is_bool ($value)) {
+			return ($value) ? 'On' : 'Off';
+		} elseif ($value === '0' || $value === '') {
+			return 'Off';
+		} elseif ($value === '1') {
+			return 'On';
+		} elseif (preg_match ('/[^a-z0-9\/\.@<> _-]/i', $value)) {
+			return '"' . $value . '"';
+		}
+		return $value;
+	};
+
+	$sections = is_array ($data[array_shift (array_keys ($data))]) ? true : false;
+
+	foreach ($data as $key => $value) {
+		if (is_array ($value)) {
+			$out .= "[$key]\n\n";
+			foreach ($value as $k => $v) {
+				$out .= str_pad ($k, 24) . '= ' . $write_value ($v) . "\n\n";
+			}
+		} else {
+			$out .= str_pad ($key, 24) . '= ' . $write_value ($value) . "\n\n";
+		}
+	}
+
+	return $out . "; */ ?>";
+}
+
+?>
+		
