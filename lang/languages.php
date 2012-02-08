2e8660bab7b400066aebc3878a9dff765ddddbe5
jbroadway <johnny@johnnybroadway.com>
2012-02-08 16:57:40 -0600

    Added initial Japanese (ja) and Chinese (zh) language files from http://www.elefantcms.com/forum/discussion/32/how-translate-elefant-cms-freamworks

--- lang/languages.php
+++ lang/languages.php
@@ -2,10 +2,29 @@
 
 [en]
 
-name		= English
-code		= en
-charset		= UTF-8
-fallback	= Off
-default		= On
+name                    = English
+code                    = en
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = On
+
+[ja]
+
+name                    = "日本語"
+code                    = ja
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = Off
+
+[zh]
+
+name                    = "简体中文"
+code                    = zh
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = Off
 
 ; */ ?>
