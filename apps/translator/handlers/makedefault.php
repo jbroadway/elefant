d8ff20ad6bbe1688e090481b3bc6d3340d467af7
jbroadway <johnny@johnnybroadway.com>
2012-02-07 22:54:15 -0600

    Added default language update handler

---
+++ apps/translator/handlers/makedefault.php
@@ -0,0 +1,23 @@
+<?php
+
+$this->require_admin ();
+
+global $i18n;
+
+require_once ('apps/translator/lib/Functions.php');
+
+if (isset ($i18n->languages[$_GET['lang']])) {
+	foreach ($i18n->languages as $key => $lang) {
+		if ($key === $_GET['lang']) {
+			$i18n->languages[$key]['default'] = true;
+		} else {
+			$i18n->languages[$key]['default'] = false;
+		}
+	}
+	file_put_contents ('lang/languages.php', translator_ini_write ($i18n->languages));
+}
+
+$this->add_notification (i18n_get ('Default language updated.'));
+$this->redirect ('/translator/index');
+
+?>
