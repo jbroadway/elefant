1ced94a424576fc2cd3c96fcdd3bb1985d5576b2
jbroadway <johnny@johnnybroadway.com>
2012-02-11 01:20:40 -0600

    Added additional stripslashes to translation builder

--- apps/translator/handlers/build.php
+++ apps/translator/handlers/build.php
@@ -63,6 +63,7 @@ foreach ($sources as $source) {
 			// parse for i18n_getf?() syntax
 			preg_match_all ('/i18n_getf? ?\([\'"](.*?)[\'"]\)/', $data, $matches);
 			foreach ($matches[1] as $str) {
+				$str = stripslashes ($str);
 				if (! isset ($list[$str])) {
 					$list[$str] = array (
 						'orig' => $str,
