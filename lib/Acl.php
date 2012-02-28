55855e308a3ad163a838acba05e75a30c9a95f88
Johnny Broadway <johnny@johnnybroadway.com>
2012-02-28 12:18:28 -0600

    Added very simple Acl class for implementing access control in custom apps

---
+++ lib/Acl.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Elefant CMS - http://www.elefantcms.com/
+ *
+ * Copyright (c) 2011 Johnny Broadway
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * A simple access control class for implementing custom permissions in your
+ * applications.
+ *
+ * Usage:
+ *
+ *   <?php
+ *   
+ *   // Default usage
+ *   $acl = new Acl ();
+ *   
+ *   if (! $acl->allowed ('resource')) {
+ *     // Keep the current user out
+ *   }
+ *   
+ *   // Use an alternate user
+ *   $user = new User ($user_id);
+ *   
+ *   if (! $acl->allowed ('resource', $user)) {
+ *     // Keep the user out
+ *   }
+ *   
+ *   ?>
+ *
+ * The format of the INI file is as follows:
+ *
+ *   [admin]
+ *   
+ *   default = On
+ *   
+ *   [editor]
+ *   
+ *   default = On
+ *   user/admin = Off
+ *   myapp = Off
+ *   
+ *   [member]
+ *   
+ *   default = Off
+ *   user = On
+ *
+ * The default lines change whether you should allow or deny by default for a
+ * given role. The naming convention `user/admin` signifies a feature within an
+ * app as opposed to the app itself.
+ *
+ * Note that Elefant doesn't use Acl in its own features, just `require_admin()`
+ * and `require_login()`. This class is intended for custom applications built on
+ * top of the framework.
+ */
+class Acl {
+	/**
+	 * The INI file that the rules were read from.
+	 */
+	public $file = 'conf/acl.php';
+
+	/**
+	 * The access control rules as an array of roles and their permissions.
+	 */
+	public $rules = array ();
+
+	/**
+	 * Constructor will call `init()` if a file is provided, or simply
+	 * set the `$rules` if an array is passed to it. With no parameters,
+	 * it will try to read `conf/acl.php` for the access list.
+	 */
+	public function __construct ($file = 'conf/acl.php') {
+		if (is_string ($file)) {
+			$this->file = $file;
+			$this->init ();
+		} elseif (is_array ($file)) {
+			$this->rules = $file;
+		}
+	}
+
+	/**
+	 * Parses the INI file and generates the rule list, adding default=false
+	 * if no default is specified for that role (deny by default).
+	 */
+	public function init () {
+		if (file_exists ($this->file)) {
+			$list = parse_ini_file ($this->file, true);
+		} else {
+			$list = parse_ini_string ($this->file, true);
+		}
+
+		foreach ($list as $role => $rules) {
+			$rules['default'] = isset ($rules['default']) ? $rules['default'] : false;
+			$this->rules[$role] = $rules;
+		}
+	}
+
+	/**
+	 * Test whether they can access a resource. If no user object is provided,
+	 * will use `User::$user->type` to determine the role.
+	 */
+	public function allowed ($resource, $user = false) {
+		$type = $user ? $user->type : User::$user->type;
+
+		return isset ($this->rules[$type][$resource])
+			? $this->rules[$type][$resource]
+			: $this->rules[$type]['default'];
+	}
+
+	/**
+	 * Add a role to the list, optionally assigning whether it should
+	 * allow or deny by default.
+	 */
+	public function add_role ($role, $default = false) {
+		$this->rules[$role] = array (
+			'default' => $default
+		);
+	}
+
+	/**
+	 * Deny a role access to the specified resource.
+	 */
+	public function deny ($role, $resource) {
+		if (! is_array ($this->rules[$role])) {
+			$this->add_role ($role);
+		}
+		$this->rules[$role][$resource] = false;
+	}
+
+	/**
+	 * Allow a role access to the specified resource.
+	 */
+	public function allow ($role, $resource) {
+		if (! is_array ($this->rules[$role])) {
+			$this->add_role ($role);
+		}
+		$this->rules[$role][$resource] = true;
+	}
+}
+
+?>
---
+++ tests/AclTest.php
@@ -0,0 +1,135 @@
+<?php
+
+require_once ('lib/Autoloader.php');
+
+class AclTest extends PHPUnit_Framework_TestCase {
+	protected static $acl;
+
+	static function setUpBeforeClass () {
+		User::$user = (object) array (
+			'type' => 'admin'
+		);
+	}
+
+	static function tearDownAfterClass () {
+		User::$user = false;
+	}
+
+	function test_init () {
+		// Test parsing INI string
+		self::$acl = new Acl ('
+			[admin]
+			default = On
+			[member]
+			default = Off
+			resource = On
+		');
+
+		// Should have two roles defined
+		$this->assertEquals (2, count (self::$acl->rules));
+
+		// Admin can access anything
+		$this->assertTrue ((bool) self::$acl->allowed ('resource'));
+	}
+
+	function test_allowed () {
+		// Test setting via array
+		self::$acl = new Acl (array (
+			'admin' => array (
+				'default' => true
+			),
+			'member' => array (
+				'default' => false,
+				'resource' => true
+			)
+		));
+
+		// Should have two roles defined
+		$this->assertEquals (2, count (self::$acl->rules));
+
+		// Admin can access anything
+		$this->assertTrue ((bool) self::$acl->allowed ('resource'));
+
+		// Change to member
+		User::$user->type = 'member';
+
+		// Member can access resource
+		$this->assertTrue ((bool) self::$acl->allowed ('resource'));
+
+		// Member cannot access anything else
+		$this->assertFalse ((bool) self::$acl->allowed ('other'));
+	}
+
+	function test_add_role () {
+		self::$acl = new Acl (array ());
+
+		// Should have no roles
+		$this->assertEquals (0, count (self::$acl->rules));
+
+		// Add a role
+		self::$acl->add_role ('member');
+
+		// Should have one role that can access nothing
+		$this->assertEquals (1, count (self::$acl->rules));
+
+		// Change to member
+		User::$user->type = 'member';
+
+		// Should be able to access nothing
+		$this->assertFalse ((bool) self::$acl->allowed ('resource'));
+
+		// Add another role that can access anything
+		self::$acl->add_role ('admin', true);
+
+		// Should have two roles
+		$this->assertEquals (2, count (self::$acl->rules));
+
+		// Change to admin
+		User::$user->type = 'admin';
+
+		// Should be able to access anything
+		$this->assertTrue ((bool) self::$acl->allowed ('resource'));
+	}
+
+	function test_deny () {
+		self::$acl = new Acl (array ());
+
+		// Should have no roles
+		$this->assertEquals (0, count (self::$acl->rules));
+
+		// Add an admin role that can access anything
+		self::$acl->add_role ('admin', true);
+
+		// Change to admin
+		User::$user->type = 'admin';
+
+		// Deny them access to a resource
+		self::$acl->deny ('admin', 'resource');
+
+		// Should be able to access anything except 'resource'
+		$this->assertFalse ((bool) self::$acl->allowed ('resource'));
+		$this->assertTrue ((bool) self::$acl->allowed ('other'));
+	}
+
+	function test_allow () {
+		self::$acl = new Acl (array ());
+
+		// Should have no roles
+		$this->assertEquals (0, count (self::$acl->rules));
+
+		// Add an admin role that can access nothing
+		self::$acl->add_role ('member', false);
+
+		// Change to admin
+		User::$user->type = 'member';
+
+		// Allow them access to a resource
+		self::$acl->allow ('member', 'resource');
+
+		// Should be able to access only 'resource'
+		$this->assertTrue ((bool) self::$acl->allowed ('resource'));
+		$this->assertFalse ((bool) self::$acl->allowed ('other'));
+	}
+}
+
+?>
