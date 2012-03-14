193c6b870ace66366e4f25644fa835680e23728d
Johnny Broadway <johnny@johnnybroadway.com>
2012-03-14 10:21:49 -0500

    Added support for ENT_SUBSTITUTE in PHP 5.4 to Template::sanitize(), added additional info about using it securely in the method's comment block

--- lib/Template.php
+++ lib/Template.php
@@ -439,9 +439,35 @@ class Template {
 
 	/**
 	 * Sanitize a value for safe output, helping to prevent XSS attacks.
+	 * Please note that this method can still be insecure if used in an
+	 * unquoted portion of an HTML tag, for example:
+	 *
+	 * Don't do this:
+	 *
+	 *     <a href="/example" {{ some_var }}>click me</a>
+	 *
+	 * But this is okay:
+	 *
+	 *     <a href="/example" id="{{ some_var }}">click me</a>
+	 *
+	 * And so is this:
+	 *
+	 *     <span id="some-var">{{ some_var }}</span>
+	 *
+	 * In the first case, if the string were to contain something like
+	 * `onclick=alert(document.cookie)` then your visitors would be
+	 * exposed to the malicious JavaScript.
+	 *
+	 * The key is to know where your data comes from, and to act accordingly.
+	 * Not all cases of the first example are necessarily a security hole,
+	 * but it should only be used if you know the source and have validated
+	 * your data beforehand.
 	 */
 	public static function sanitize ($val, $charset = 'UTF-8') {
-		return htmlspecialchars ($val, ENT_QUOTES | ENT_IGNORE, $charset);
+		if (! defined ('ENT_SUBSTITUTE')) {
+			define ('ENT_SUBSTITUTE', ENT_IGNORE);
+		}
+		return htmlspecialchars ($val, ENT_QUOTES | ENT_SUBSTITUTE, $charset);
 	}
 
 	/**
