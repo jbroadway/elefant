6141f4c1728231fca3d02613fc25e7c6cd4b7c6d
Jared Short <jaredlshort@gmail.com>
2012-05-01 00:43:53 -0400

    Updated Dynamic Objects. Added Columning. Added Twitter feed, Facebook like-box.

---
+++ apps/social/handlers/twitter/feed.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Embeds a twitter feed widget into the current page. Used by
+ * the WYSIWYG editor's dynamic objects menu.
+ */
+
+if (! isset (self::$called['social/twitter/init'])) {
+	echo $this->run ('social/twitter/init');
+}
+
+if (! isset ($data['twitter_id'])) {
+	$data['twitter_id'] = $appconf['Twitter']['id'];
+}
+
+echo $tpl->render ('social/twitter/feed', $data);
+
+?>
