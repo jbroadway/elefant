a289ae151084d919e8829a6834fb4955aead6eb6
Mike Melnyk <github@melnyk.org>
2012-09-10 14:05:06 -0700

    Updated date format and language code for Ukrainian and Russian languages

--- lang/languages.php
+++ lang/languages.php
@@ -18,28 +18,10 @@ charset                 = UTF-8
 fallback                = Off
 default                 = Off
 
-[ja]
-
-name                    = "日本語"
-code                    = ja
-locale                  = Off
-charset                 = UTF-8
-fallback                = Off
-default                 = Off
-
 [nl]
 
 name                    = Nederlands
 code                    = nl
-charset                 = UTF-8
-fallback                = Off
-default                 = Off
-
-[zh]
-
-name                    = "简体中文"
-code                    = zh
-locale                  = Off
 charset                 = UTF-8
 fallback                = Off
 default                 = Off
@@ -56,28 +38,46 @@ date_format             = d.mmmm yyyy
 short_format            = dd.mm.yyyy
 time_format             = "HH:MM"
 
-[ua]
-
-name                    = "Українська"
-code                    = ua
-charset                 = UTF-8
-fallback                = Off
-default                 = Off
-locale                  = Off
-date_format             = d.mmmm yyyy
-short_format            = dd.mm.yyyy
-time_format             = "HH:MM"
-
 [ru]
 
 name                    = "Русский"
 code                    = ru
+locale                  = Off
 charset                 = UTF-8
 fallback                = Off
 default                 = Off
-locale                  = Off
-date_format             = d.mmmm yyyy
-short_format            = dd.mm.yyyy
+date_format             = dd mmmm yyyy
+short_format            = dd mmm
 time_format             = "HH:MM"
+
+[uk]
+
+name                    = "Українська"
+code                    = uk
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = Off
+date_format             = dd mmmm yyyy
+short_format            = dd mmm
+time_format             = "HH:mm"
+
+[ja]
+
+name                    = "日本語"
+code                    = ja
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = Off
+
+[zh]
+
+name                    = "简体中文"
+code                    = zh
+locale                  = Off
+charset                 = UTF-8
+fallback                = Off
+default                 = Off
 
 ; */ ?>
