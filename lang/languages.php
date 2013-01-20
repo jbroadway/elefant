e02a4186b7dcf765a713699bd715056837d50d96
jbroadway <johnny@johnnybroadway.com>
2013-01-20 16:25:10 -0600

    Italian translation, thanks Alpi

--- lang/languages.php
+++ lang/languages.php
@@ -29,6 +29,14 @@ date_format             = d mmmm yyyy
 short_format            = d mmm
 time_format             = "HH:MM"
 
+[it]
+
+name                    = Italiano
+code                    = it
+locale                  = Off
+charset                 = UTF-8
+default                 = Off
+
 [nl]
 
 name                    = Nederlands
