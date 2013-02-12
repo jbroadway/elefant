3ce45af24d1957d3bf79cd909b40cd2661816ca3
jbroadway <johnny@johnnybroadway.com>
2013-02-12 11:26:46 -0600

    Moved Redactor initializations into admin/util/redactor, with Elefant integrations in admin/util/wysiwyg

--- apps/admin/handlers/util/wysiwyg.php
+++ apps/admin/handlers/util/wysiwyg.php
@@ -15,14 +15,12 @@
 $this->run ('admin/util/fontawesome');
 $this->run ('admin/util/dynamicobjects');
 $this->run ('filemanager/util/browser');
+$this->run ('admin/util/redactor');
 
 $page->add_style ('/js/jquery-ui/jquery-ui.css');
-$page->add_style ('/apps/admin/js/redactor/redactor.css');
-$page->add_style ('/apps/admin/js/redactor/custom.css');
 
 $page->add_script ('/js/jquery-ui/jquery-ui.min.js');
 $page->add_script ('/js/jquery.quickpager.js');
-$page->add_script ('/apps/admin/js/redactor/redactor.min.js');
 $page->add_script ('/apps/admin/js/redactor/plugins/filebrowser.js');
 $page->add_script ('/apps/admin/js/redactor/plugins/imagebrowser.js');
 $page->add_script ('/apps/admin/js/redactor/plugins/dynamic.js');
