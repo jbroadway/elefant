5ac6fbd45082d79d1f5d50062f9134917bff95f4
gareththered <gareth@garethwilliams.me.uk>
2013-04-14 21:59:43 +0100

    Fixed typo in RegExp line of multi-file.js

--- apps/filemanager/js/jquery.multi-file.js
+++ apps/filemanager/js/jquery.multi-file.js
@@ -35,7 +35,7 @@
 
 		for (var i in new_files) {
 			file = new_files[i];
-			self.last_path = self.dirname (file).replace (RegExp('^\/' + self.escape_RegExp()(filemanager_path,'/') + '\/?'), '');
+			self.last_path = self.dirname (file).replace (RegExp('^\/' + self.escape_RegExp(filemanager_path,'/') + '\/?'), '');
 
 			// avoid duplicates
 			if ($.inArray (file, files) === -1) {
