abd03e04f5888ca83e61bd63e5d0418779ace4bb
jbroadway <johnny@johnnybroadway.com>
2013-04-20 10:13:47 -0500

    Started adding acl-based permission checking

--- apps/designer/handlers/index.php
+++ apps/designer/handlers/index.php
@@ -6,9 +6,7 @@
 
 $page->layout = 'admin';
 
-if (! User::require_admin ()) {
-	$this->redirect ('/admin');
-}
+$this->require_acl ('admin', 'designer');
 
 $lock = new Lock ();
 
--- apps/user/conf/acl.php
+++ apps/user/conf/acl.php
@@ -1,6 +1,6 @@
 ; <?php /*
 
-default = "Default access for this role (selected means yes by default)"
+default = "Default access for this role (selected means allow by default)"
 content/member = "View members-only content"
 content/private = "View private content"
 user = "Add/remove users"
