5867cf0fd93a49726d65fdf3432f5b2c1e569230
jbroadway <johnny@johnnybroadway.com>
2013-04-21 00:01:19 -0500

    Added role delete, closes #162

---
+++ apps/user/handlers/role/delete.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Deletes a role.
+ */
+
+$this->require_acl ('admin', 'user', 'user/roles');
+
+$page->layout = 'admin';
+
+if (! isset ($_POST['role'])) {
+	$this->redirect ('/user/roles');
+}
+
+$rules = User::acl ()->rules;
+unset ($rules[$_POST['role']]);
+
+if (! Ini::write ($rules, conf ('Paths', 'access_control_list'))) {
+	$this->add_notification (__ ('Unable to save the file.'));
+} else {
+	$this->add_notification (__ ('Role deleted.'));
+}
+$this->redirect ('/user/roles');
+
+?>
