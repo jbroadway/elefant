a4d09497f9f608d43000b2542a83b36a951ccebe
jbroadway <johnny@johnnybroadway.com>
2013-06-07 14:38:05 -0500

    Created database fixtures for Navigation test

--- tests/NavigationTest.php
+++ tests/NavigationTest.php
@@ -1,6 +1,30 @@
 <?php
 
 class NavigationTest extends PHPUnit_Framework_TestCase {
+	static function setUpBeforeClass () {
+		DB::open (array ('master' => true, 'driver' => 'sqlite', 'file' => ':memory:'));
+		DB::$prefix = 'elefant_';
+		$sql = sql_split ('
+			create table #prefix#webpage (
+				id char(72) not null primary key,
+				title char(72) not null,
+				menu_title char(72) not null,
+				window_title char(72) not null,
+				access char(12) not null,
+				layout char(48) not null,
+				description text,
+				keywords text,
+				body text
+			);
+			insert into #prefix#webpage (id, title, menu_title, window_title, access, layout, description, keywords, body) values ("index", "Welcome to Elefant", "Home", "", "public", "default", "", "", \'<table><tbody><tr><td><h3>Congratulations!</h3>You have successfully installed Elefant, the refreshingly simple new PHP web framework and CMS.</td><td><h3>Getting Started</h3>To log in as an administrator and edit pages, write a blog post, or upload files, go to <a href="/admin">/admin</a>.</td><td><h3>Developers</h3>Documentation, source code and issue tracking can be found at <a href="http://github.com/jbroadway/elefant">github.com/jbroadway/elefant</a></td></tr></tbody></table>\');
+		');
+		foreach ($sql as $query) {
+			if (! DB::execute ($query)) {
+				die (DB::error ());
+			}
+		}
+	}
+
 	function test_single_node () {
 		$n = new Navigation;
 
