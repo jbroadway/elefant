afad99086b1ed776b0bfb113f2059faf359862ac
Johnny Broadway <johnny@johnnybroadway.com>
2014-01-07 13:13:30 -0600

    Added helpers file to filemanager

---
+++ apps/filemanager/conf/helpers.php
@@ -0,0 +1,10 @@
+; <?php /*
+
+filemanager/audio = 1
+filemanager/gallery = 1
+filemanager/slideshow = 1
+filemanager/swf = 1
+filemanager/video = 1
+filemanager/util/browser = 1
+filemanager/util/multi-file = 1
+filemanager/util/multi-image = 1
