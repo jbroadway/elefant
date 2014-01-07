e46929150dc6ad53ce36e5cd03599daa7ec1f117
Johnny Broadway <johnny@johnnybroadway.com>
2014-01-07 13:52:03 -0600

    Added helpers file to social app

---
+++ apps/social/conf/helpers.php
@@ -0,0 +1,13 @@
+; <?php /*
+
+social/facebook/comments = 1
+social/facebook/commentcount = 1
+social/facebook/like = 1
+social/facebook/like-box = 1
+social/google/maps = 1
+social/google/plusone = 1
+social/twitter/feed = 1
+social/twitter/follow = 1
+social/twitter/tweet = 1
+social/video/vimeo = 1
+social/video/youtube = 1
