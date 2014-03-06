0ca42c9c5437cacd056ba48617c26f54217ce6d7
jbroadway <johnny@johnnybroadway.com>
2014-03-05 19:45:58 -0600

    Updated Analog to 1.0.4

--- lib/vendor/Analog/Logger.php
+++ lib/vendor/Analog/Logger.php
@@ -103,9 +103,22 @@ class Logger implements LoggerInterface {
 	 * Interpolates context values into the message placeholders.
 	 */
 	private function interpolate ($message, array $context = array ()) {
+		if (is_array ($message)) {
+			return $message;
+		}
+
 		// build a replacement array with braces around the context keys
 		$replace = array ();
 		foreach ($context as $key => $val) {
+			if (is_object ($val) && get_class ($val) === 'DateTime') {
+				$val = $val->format ('Y-m-d H:i:s');
+			} elseif (is_object ($val)) {
+				$val = json_encode ($val);
+			} elseif (is_array ($val)) {
+				$val = json_encode ($val);
+			} elseif (is_resource ($val)) {
+				$val = (string) $val;
+			}
 			$replace['{' . $key . '}'] = $val;
 		}
 		
