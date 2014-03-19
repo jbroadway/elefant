47d9f335c90efffc8fdff9cfa5c18e2e69cfd3e8
Johnny Broadway <johnny@johnnybroadway.com>
2014-03-19 16:15:03 -0500

    Added outline of ./elefant update all and ./elefant update <app-name>

--- apps/cli/handlers/update.php
+++ apps/cli/handlers/update.php
@@ -15,119 +15,147 @@ if (! $this->cli) {
 
 $page->layout = false;
 
-require_once ('apps/cli/lib/Functions.php');
-
-// get the major/minor version
-$major_minor = preg_replace ('/\.[0-9]+$/', '', ELEFANT_VERSION);
-
-// fetch the latest version from the server
-$data = Updater::fetch ('releases/' . $major_minor . '.json');
-if (! $data) {
-	Cli::out ('Error: ' . Updater::error () . ' releases/' . $major_minor . '.json', 'error');
-	return;
-}
-$res = json_decode ($data);
-
-if (! is_object ($res)) {
-	Cli::out ('Error: Unable to fetch latest version from the server.');
-	return;
-}
-
-// are we already up to date?
-if ($res->latest <= ELEFANT_VERSION) {
-	echo ELEFANT_VERSION . " is already up-to-date.\n";
-	return;
-}
-
-// new version ready
-$latest = $res->latest;
-echo "New version: {$latest}\n";
-
-// make sure conf/updates exists
-if (! file_exists ('conf/updates')) {
-	mkdir ('conf/updates');
-}
-
-// check for and download new patch files
-$data = Updater::fetch ('patches.json');
-if (! $data) {
-	Cli::out ('Error: ' . Updater::error () . ' patches.json', 'error');
-	return;
-}
-$res = json_decode ($data);
-
-if (! is_object ($res)) {
-	Cli::out ('Error: Unable to fetch patch list from the server.', 'error');
-	return;
-}
-
-foreach ($res->patches as $patch_file) {
-	$base = basename ($patch_file);
-	if (! file_exists ('conf/updates/' . $base)) {
-		echo "Fetching new patch: {$base}\n";
-		$contents = Updater::fetch ($patch_file);
-		if (! $contents) {
-			Cli::out ('Error: ' . Updater::error () . ' ' . $patch_file, 'error');
-			return;
+// Build a list of apps to upgrade after the core has been upgraded first.
+$app = 'none';
+$update_apps = array ();
+if (isset ($_SERVER['argv'][2])) {
+	$app = $_SERVER['argv'][2];
+	
+	if ($app === 'all') {
+		$files = glob ('apps/*/elefant.json');
+		
+		foreach ($files as $file) {
+			$info = explode ('/', $file);
+			$update_apps[] = $info[1];
 		}
-		file_put_contents ('conf/updates/' . $base, $contents);
-
-		// TODO: MD5 checks from 3rd party repo
+	} elseif (strpos ($app, '..') !== false || ! file_exists ('apps/' . $app . '/elefant.json')) {
+		// App not found or invalid
+		Cli::out ('Invalid app name: ' . $app, 'error');
+		return;
+	} else {
+		$update_apps[] = $app;
 	}
+
+	Cli::out ('Error: Not implemented yet.', 'error');
+	return;
 }
 
-foreach ($res->scripts as $script_file) {
-	$base = basename ($script_file);
-	if (! file_exists ('conf/updates/' . $base)) {
-		echo "Fetching new db update: {$base}\n";
-		$contents = Updater::fetch ($script_file);
-		if (! $contents) {
-			Cli::out ('Error: ' . Updater::error () . ' ' . $script_file, 'error');
-			return;
-		}
-		file_put_contents ('conf/updates/' . $base, $contents);
+if ($app === 'none' || $app === 'all') {
+	require_once ('apps/cli/lib/Functions.php');
 
-		// TODO: MD5 checks from 3rd party repo
+	// Get the major/minor version
+	$major_minor = preg_replace ('/\.[0-9]+$/', '', ELEFANT_VERSION);
+
+	// Fetch the latest version from the server
+	$data = Updater::fetch ('releases/' . $major_minor . '.json');
+	if (! $data) {
+		Cli::out ('Error: ' . Updater::error () . ' releases/' . $major_minor . '.json', 'error');
+		return;
 	}
-}
+	$res = json_decode ($data);
 
-$versions = cli_get_versions (ELEFANT_VERSION, $latest);
-
-// Test and apply the patches and db updates in sequence
-foreach ($versions as $version) {
-	printf ("Testing patch: %s\n", basename ($version['patch']));
-	exec ('patch --dry-run -p1 -f -i ' . $version['patch'], $output);
-	$output = join ("\n", $output);
-	if (strpos ($output, 'FAILED')) {
-		Cli::out ('Error applying patch ' . $version['patch'], 'error');
-		echo "See conf/updates/error.log for details.\n";
-		file_put_contents ('conf/updates/error.log', $output);
+	if (! is_object ($res)) {
+		Cli::out ('Error: Unable to fetch latest version from the server.', 'error');
 		return;
 	}
 
-	// Patch is okay to apply
-	echo "Patch ok, applying...\n";
-	exec ('patch -p1 -f -i ' . $version['patch']);
+	// Are we already up to date?
+	if ($res->latest <= ELEFANT_VERSION) {
+		echo ELEFANT_VERSION . " is already up-to-date.\n";
+		return;
+	}
 
-	// Apply associated database updates
-	if ($version['script']) {
-		printf ("Applying db update: %s\n", basename ($version['script']));
-		$sqldata = sql_split (file_get_contents ($version['script']));
-		DB::beginTransaction ();
-		foreach ($sqldata as $sql) {
-			if (! DB::execute ($sql)) {
-				$error = DB::error ();
-				DB::rollback ();
-				Cli::out ('Error applying db update: ' . $version['script'], 'error');
-				echo "See conf/updates/error.log for details.\n";
-				file_put_contents ('conf/updates/error.log', $error);
+	// New version ready
+	$latest = $res->latest;
+	echo "New version: {$latest}\n";
+
+	// Make sure conf/updates exists
+	if (! file_exists ('conf/updates')) {
+		mkdir ('conf/updates');
+	}
+
+	// Check for and download new patch files
+	$data = Updater::fetch ('patches.json');
+	if (! $data) {
+		Cli::out ('Error: ' . Updater::error () . ' patches.json', 'error');
+		return;
+	}
+	$res = json_decode ($data);
+
+	if (! is_object ($res)) {
+		Cli::out ('Error: Unable to fetch patch list from the server.', 'error');
+		return;
+	}
+
+	foreach ($res->patches as $patch_file) {
+		$base = basename ($patch_file);
+		if (! file_exists ('conf/updates/' . $base)) {
+			echo "Fetching new patch: {$base}\n";
+			$contents = Updater::fetch ($patch_file);
+			if (! $contents) {
+				Cli::out ('Error: ' . Updater::error () . ' ' . $patch_file, 'error');
 				return;
 			}
+			file_put_contents ('conf/updates/' . $base, $contents);
 		}
-		DB::commit ();
 	}
+
+	foreach ($res->scripts as $script_file) {
+		$base = basename ($script_file);
+		if (! file_exists ('conf/updates/' . $base)) {
+			echo "Fetching new db update: {$base}\n";
+			$contents = Updater::fetch ($script_file);
+			if (! $contents) {
+				Cli::out ('Error: ' . Updater::error () . ' ' . $script_file, 'error');
+				return;
+			}
+			file_put_contents ('conf/updates/' . $base, $contents);
+		}
+	}
+
+	$versions = cli_get_versions (ELEFANT_VERSION, $latest);
+
+	// Test and apply the patches and db updates in sequence
+	foreach ($versions as $version) {
+		printf ("Testing patch: %s\n", basename ($version['patch']));
+		exec ('patch --dry-run -p1 -f -i ' . $version['patch'], $output);
+		$output = join ("\n", $output);
+		if (strpos ($output, 'FAILED')) {
+			Cli::out ('Error applying patch ' . $version['patch'], 'error');
+			echo "See conf/updates/error.log for details.\n";
+			file_put_contents ('conf/updates/error.log', $output);
+			return;
+		}
+
+		// Patch is okay to apply
+		echo "Patch ok, applying...\n";
+		exec ('patch -p1 -f -i ' . $version['patch']);
+
+		// Apply associated database updates
+		if ($version['script']) {
+			printf ("Applying db update: %s\n", basename ($version['script']));
+			$sqldata = sql_split (file_get_contents ($version['script']));
+			DB::beginTransaction ();
+			foreach ($sqldata as $sql) {
+				if (! DB::execute ($sql)) {
+					$error = DB::error ();
+					DB::rollback ();
+					Cli::out ('Error applying db update: ' . $version['script'], 'error');
+					echo "See conf/updates/error.log for details.\n";
+					file_put_contents ('conf/updates/error.log', $error);
+					return;
+				}
+			}
+			DB::commit ();
+		}
+	}
+
+	Cli::out (sprintf ("Applied %d updates.", count ($versions)), 'success');
 }
 
-Cli::out (sprintf ("Applied %d updates.", count ($versions)), 'success');
+// Upgrade the apps that were specified as well
+foreach ($update_apps as $app) {
+	// TODO: Implement me
+}
 
 ?>
