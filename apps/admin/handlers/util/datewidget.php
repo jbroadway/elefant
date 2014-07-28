bbe9a82b1da805e7c0fcde8610f60b91e23b5aef
jbroadway <johnny@johnnybroadway.com>
2014-07-28 15:57:17 -0500

    Added missing jQuery UI to admin/util/datewidget helper

--- apps/admin/handlers/util/datewidget.php
+++ apps/admin/handlers/util/datewidget.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Call this to turn text inputs with a class="date" or
- * class="datetime" into date and date/time selectors.
+ * Call this to turn text inputs with a class="datewidget-date" or
+ * class="datewidget-datetime" into date and date/time selectors.
  *
  * In PHP code, call it like this:
  *
@@ -13,7 +13,9 @@
  *     {! admin/util/datewidget !}
  */
 
+$page->add_style ('/js/jquery-ui/jquery-ui.css');
 $page->add_style ('/apps/admin/css/datewidget.css');
+$page->add_script ('/js/jquery-ui/jquery-ui.min.js');
 $page->add_script ('/apps/blog/js/jquery.timepicker.js');
 $page->add_script ('/apps/admin/js/datewidget.js');
 
