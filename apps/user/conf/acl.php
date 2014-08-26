d415a45dca698adc3c77f68fb5bedfc1a61baa1b
jbroadway <johnny@johnnybroadway.com>
2014-08-26 12:27:16 -0500

    Added new permission for updating which role a member belongs to

--- apps/user/conf/acl.php
+++ apps/user/conf/acl.php
@@ -3,7 +3,8 @@
 default = "Default access for this role (selected means allow by default)"
 content/member = "View members-only content"
 content/private = "View private content"
-user = "Modify user accounts"
-user/roles = "Modify user roles and permissions"
+user = "Modify member accounts"
+user/roles = "Modify permissions of member roles"
+user/edit_roles = "Assign and change member roles"
 
 ; */ ?>
