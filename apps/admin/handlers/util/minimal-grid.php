dfb08081c03e01210bb13e7577de19217e06017a
jbroadway <johnny@johnnybroadway.com>
2014-11-04 11:43:49 -0600

    Added minimal responsive grid for apps to consistently render multi-column content

---
+++ apps/admin/handlers/util/minimal-grid.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Includes a minimal responsive grid for layout out an app's
+ * output, e.g., cards or tiles, for consistent display across
+ * websites.
+ *
+ * In PHP code, call it like this:
+ *
+ *     $this->run ('admin/util/minimal-grid');
+ *
+ * In a view template, call it like this:
+ *
+ *     {! admin/util/minimal-grid !}
+ */
+
+$page->add_script ('/apps/admin/css/minimal-grid.css');
