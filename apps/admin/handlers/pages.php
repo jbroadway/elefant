00539354b2b8cfb173e751472f9381b7ae53c01b
jbroadway <johnny@johnnybroadway.com>
2014-11-21 16:37:42 -0600

    Added search to web pages admin

--- apps/admin/handlers/pages.php
+++ apps/admin/handlers/pages.php
@@ -12,13 +12,21 @@ $page->title = __ ('All Pages');
 $limit = 20;
 $num = isset ($_GET['offset']) ? $_GET['offset'] : 1;
 $offset = ($num - 1) * $limit;
+$q = isset ($_GET['q']) ? $_GET['q'] : '';
+$url = ! empty ($q)
+	? '/admin/pages?q=' . urlencode ($q) . '&offset=%d'
+	: '/admin/pages?offset=%d';
 
 $lock = new Lock ();
 
 $pages = Webpage::query ('id, title, access')
+	->where_search ($q, array ('id', 'title', 'menu_title', 'window_title', 'access', 'keywords', 'description', 'body'))
 	->order ('title asc')
 	->fetch_orig ($limit, $offset);
-$count = Webpage::query ()->count ();
+
+$count = Webpage::query ()
+	->where_search ($q, array ('id', 'title', 'menu_title', 'window_title', 'access', 'keywords', 'description', 'body'))
+	->count ();
 
 foreach ($pages as $k => $p) {
 	$pages[$k]->locked = $lock->exists ('Webpage', $p->id);
@@ -29,5 +37,6 @@ echo $tpl->render ('admin/pages', array (
 	'total' => $count,
 	'pages' => $pages,
 	'count' => count ($pages),
-	'url' => '/admin/pages?offset=%d'
+	'url' => $url,
+	'q' => $q
 ));
