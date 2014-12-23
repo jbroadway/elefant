8d8ed59c3f1f2c6d999b9541b810808101be9090
John Luxford <john@campfireunion.com>
2014-12-23 17:41:32 -0600

    Updated google maps handler to work with grid editor dynamic script loading

--- apps/social/handlers/google/maps.php
+++ apps/social/handlers/google/maps.php
@@ -5,13 +5,14 @@
  * the WYSIWYG editor's dynamic objects menu.
  */
 
-$page->add_script ('//maps.googleapis.com/maps/api/js?sensor=false');
-
 $data['map_id'] = rand ();
 
 $data['width'] = (isset ($data['width']) && ! empty ($data['width'])) ? $data['width'] : '100%';
 $data['height'] = (isset ($data['height']) && ! empty ($data['height'])) ? $data['height'] : '400px';
 $data['width'] = is_numeric ($data['width']) ? $data['width'] . 'px' : $data['width'];
 $data['height'] = is_numeric ($data['height']) ? $data['height'] . 'px' : $data['height'];
+
+$page->add_script ($tpl->render ('social/google/maps_loader', $data));
+$page->add_script ($tpl->render ('social/google/maps_script', $data));
 
 echo $tpl->render ('social/google/maps', $data);
