7d773dcbc1761dcd4aa7808ffa7bb505c9b0911a
John Luxford <john@campfireunion.com>
2014-12-26 15:04:23 -0600

    Moved $.i18n() to its own script and added admin/util/i18n helper for non-admin use of I18n in JavaScript scripts

--- apps/admin/js/top-bar.js
+++ apps/admin/js/top-bar.js
@@ -119,38 +119,6 @@ jQuery.expanded_section = function (options) {
 };
 
 /**
- * Translates strings of text in .js files.
- * Usage:
- *
- *     <script>
- *     $(function () {
- *         // This will make the text available to $.i18n()
- *         $.i18n_append ({
- *             'Some text': '{"Some text"}',
- *             'Another string': '{"Another string"}'
- *         });
- *         
- *         // Now fetch a translated string
- *         console.log ($.i18n ('Some text'));
- *     });
- *     </script>
- */
-(function ($) {
-	var strings = {};
-
-	$.i18n_append = function (obj) {
-		strings = $.extend (strings, obj);
-	};
-
-	$.i18n = function (text) {
-		if (strings[text]) {
-			return strings[text];
-		}
-		return text;
-	};
-})(jQuery);
-
-/**
  * Displays the update notice if one is available. This is a JSONP
  * response handler.
  */
