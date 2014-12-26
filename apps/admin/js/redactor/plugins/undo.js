bd3b3c87bcc3e545120b055efa2a8ea963103a25
John Luxford <john@campfireunion.com>
2014-12-26 15:18:21 -0600

    Moved undo to separate plugin

---
+++ apps/admin/js/redactor/plugins/undo.js
@@ -0,0 +1,15 @@
+if (! RedactorPlugins) var RedactorPlugins = {};
+
+RedactorPlugins.undo = function () {
+	return {
+		init: function () {
+			var dropdown = {};
+			
+			dropdown.point1 = { title: $.i18n ('Undo'), func: this.buffer.undo };
+			dropdown.point2 = { title: $.i18n ('Redo'), func: this.buffer.redo };
+			
+			var button = this.button.add ('undo', $.i18n ('Undo'));
+			this.button.addDropdown (button, dropdown);
+		}
+	};
+};
