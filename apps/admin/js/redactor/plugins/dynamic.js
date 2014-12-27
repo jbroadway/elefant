08e5a6da8f4b573061e1c9582e80e9fceed42297
Johnny Broadway <johnny@johnnybroadway.com>
2014-12-26 22:05:39 -0600

    Dynamic Objects plugin updated for Redactor 10

--- apps/admin/js/redactor/plugins/dynamic.js
+++ apps/admin/js/redactor/plugins/dynamic.js
@@ -12,42 +12,42 @@ RedactorPlugins.dynamic = function () {
 		init: function () {
 			var button = this.button.add ('dynamic', $.i18n ('Dynamic Objects'));
 			this.button.setAwesome ('dynamic', 'fa-cog');
-			this.button.addCallback (button, $.proxy (this.dynamic.add_handler, this));
-			$('.redactor-editor').on ('click', '.embedded', {plugin: this}, $.proxy (this.dynamic.edit_handler, this));
+			this.button.addCallback (button, this.dynamic.add_handler);
+			$('.redactor-editor').on ('click', '.embedded', $.proxy (this.dynamic.edit_handler, this));
 		},
 
 		// Open the dialog when the button is clicked
 		add_handler: function (button, el, self, evt, current) {
-			this._current = current ? this._current : null;
-			this.selectionSave ();
+			this.dynamic._current = current ? this.dynamic._current : null;
+			this.selection.save ();
 			$.dynamicobjects ({
-				callback: $.proxy (this.insert_object, this),
+				callback: $.proxy (this.dynamic.insert_object, this),
 				current: current ? current : null
 			});
 		},
 
 		// Reopen the dialog to edit an existing embed
 		edit_handler: function (evt) {
-			this._current = evt.target;
-			this.add_handler ('dynamic', evt.target, this, evt, $(evt.target).data ('embed'));
+			this.dynamic._current = evt.target;
+			this.dynamic.add_handler ('dynamic', evt.target, this, evt, $(evt.target).attr ('data-embed'));
 			return false;
 		},
 
 		// Insert/replace an embed code in the editor
 		insert_object: function (embed_code, handler, params, label) {
-			this.selectionRestore ();
-			this.bufferSet ();
+			this.selection.restore ();
+			this.buffer.set ();
 
-			if (this._current) {
+			if (this.dynamic._current) {
 				// update existing embed
-				$(this._current)
+				$(this.dynamic._current)
 					.attr ('data-embed', embed_code)
 					.attr ('data-label', label);
 
-				this.sync();
+				this.code.sync();
 			} else {
 				// enter a new embed
-				this.insertHtml (
+				this.insert.htmlWithoutClean (
 					'<span class="embedded" data-embed="' + embed_code + '" data-label="' + label + '" title="Click to edit."></span>'
 				);
 			}
