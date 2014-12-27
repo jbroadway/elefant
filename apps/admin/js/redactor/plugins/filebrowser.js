8538f3ed377f9834f5865ed90e0eaeb66a835385
Johnny Broadway <johnny@johnnybroadway.com>
2014-12-26 22:05:39 -0600

    Dynamic Objects plugin updated for Redactor 10

--- apps/admin/js/redactor/plugins/filebrowser.js
+++ apps/admin/js/redactor/plugins/filebrowser.js
@@ -28,11 +28,11 @@ RedactorPlugins.filebrowser = function () {
 			if (file.match (/\.(jpg|png|gif)$/i)) {
 				this.insert.html ('<img src="' + file + '" alt="" style="" />');
 			} else if (file.match (/\.swf$/i)) {
-				this.insert.html ('<span class="embedded" data-embed="filemanager/swf?file=' + file + '" data-label="Embedded Flash (SWF)" title="Click to edit."></span>');
+				this.insert.htmlWithoutClean ('<span class="embedded" data-embed="filemanager/swf?file=' + file + '" data-label="Embedded Flash (SWF)" title="Click to edit."></span>');
 			} else if (file.match (/\.(mp4|m4v|flv|f4v)$/i)) {
-				this.insert.html ('<span class="embedded" data-embed="filemanager/video?file=' + file + '" data-label="Embedded Video (MP4)" title="Click to edit."></span>');
+				this.insert.htmlWithoutClean ('<span class="embedded" data-embed="filemanager/video?file=' + file + '" data-label="Embedded Video (MP4)" title="Click to edit."></span>');
 			} else if (file.match (/\.mp3$/i)) {
-				this.insert.html ('<span class="embedded" data-embed="filemanager/audio?file=' + file + '" data-label="Embedded Audio (MP3)" title="Click to edit."></span>');
+				this.insert.htmlWithoutClean ('<span class="embedded" data-embed="filemanager/audio?file=' + file + '" data-label="Embedded Audio (MP3)" title="Click to edit."></span>');
 			} else {
 				var basename = file.match (/([^\/]+)$/)[1];
 				this.insert.html ('<a href="' + file + '">' + basename + '</a>');
