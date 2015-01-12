fe9a4b4922152d70d2193901d28e2df720f845eb
Johnny Broadway <johnny@johnnybroadway.com>
2015-01-12 16:18:37 -0600

    Grid enable/disable for page body

--- apps/admin/forms/add.php
+++ apps/admin/forms/add.php
@@ -7,17 +7,9 @@ regex = "/^[a-zA0-9_-]+$/"
 unique = "#prefix#webpage.id"
 not exists = "apps"
 
-[title]
-
-not empty = 1
-
 [layout]
 
 skip_if_empty = 1
 callback = "admin\Layout::exists"
-
-[body]
-
-not empty = 1
 
 ; */ ?>
--- apps/admin/forms/edit.php
+++ apps/admin/forms/edit.php
@@ -6,17 +6,9 @@ not empty = 1
 regex = "/^[a-zA0-9_-]+$/"
 not exists = "apps"
 
-[title]
-
-not empty = 1
-
 [layout]
 
 skip_if_empty = 1
 callback = "admin\Layout::exists"
-
-[body]
-
-not empty = 1
 
 ; */ ?>
