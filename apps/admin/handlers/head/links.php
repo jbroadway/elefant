1b394af904d7dd256448afb87fa551a16edef759
BronyBorn <minecraftsolutionsinc@gmail.com>
2015-01-14 03:50:58 -0600

    Fix from merge

--- apps/admin/handlers/head/links.php
+++ apps/admin/handlers/head/links.php
@@ -9,27 +9,26 @@
 $page->layout = false;
 header ('Content-Type: application/json');
 
-if (! User::require_admin ()) {
-	return;
-}
+if (! User::require_admin ()) return;
 
-admin\Toolbar::set_file (conf ('Paths', 'toolbar'));
+$tools = admin\Toolbar::tools ($this);
+if (count($tools) === 0) {
+	$tools = admin\Toolbar::apps($this);
+	$is_apps = true;
+} else $is_apps = false;
 
-$custom_tools = admin\Toolbar::custom_tools ($this);
-if ($custom_tools === false) {
-	$tools = admin\Toolbar::parse_apps ($this);
-} else {
-	$tools = $custom_tools;
-	$custom_tools = true;
-}
+$editable = User::require_acl('admin/toolbar');
 
 $out = array (
 	'name' => Product::name (),
 	'logo' => Product::logo_toolbar (),
+	'is_apps' => $is_apps,
+	'editable' => $editable,
 	'links' => $tpl->render ('admin/head/links', array (
 		'user' => User::val ('name'),
 		'tools' => $tools,
-		'custom' => $custom_tools
+		'is_apps' => $is_apps,
+		'editable' => $editable
 	))
 );
 
