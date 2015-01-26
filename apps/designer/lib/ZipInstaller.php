f94f6172329a8b368411355d1224d3bf364ff521
Oleg Ivanchenko <oleg.ivanchenko@iru.org>
2015-01-26 10:18:36 +0100

    cURL calls: Proxy configuration added.

--- apps/designer/lib/ZipInstaller.php
+++ apps/designer/lib/ZipInstaller.php
@@ -91,6 +91,7 @@ class ZipInstaller extends Installer {
 			curl_setopt ($ch, CURLOPT_SSL_VERIFYHOST, 0);
 			curl_setopt ($ch, CURLOPT_FAILONERROR, 0);
 			curl_setopt ($ch, CURLOPT_URL, $url);
+			curl_setproxy ($ch, $url);
 			$res = curl_exec ($ch);
 			curl_close ($ch);
 		} else {
