093341a87d482e28bb351aaa528b462df3d0be59
Oleg Ivanchenko <oleg.ivanchenko@iru.org>
2015-02-20 09:18:17 +0100

    Blog: Wordpress import: Blogs and pages post types are used for migration. Pages creation added. Old URLs redirection added.

--- apps/blog/handlers/import/wordpress.php
+++ apps/blog/handlers/import/wordpress.php
@@ -31,7 +31,9 @@ if ($f->submit ()) {
 	if (move_uploaded_file ($_FILES['import_file']['tmp_name'], 'cache/blog_' . $_FILES['import_file']['name'])) {
 		$file = 'cache/blog_' . $_FILES['import_file']['name'];
 		
-		$imported = 0;
+		$imported_posts = $imported_pages = 0;
+		
+		$links = array ();
 
 		try {
 			$posts = new SimpleXMLElement (file_get_contents ($file));
@@ -40,52 +42,99 @@ if ($f->submit ()) {
 				$dc = $entry->children ('http://purl.org/dc/elements/1.1/');
 				$content = $entry->children ('http://purl.org/rss/1.0/modules/content/');
 				$wp = $entry->children ('http://wordpress.org/export/1.2/');
-				$published = $wp->status == 'publish' ? 'yes' : 'no';
+				
+				switch ($wp->post_type) {
+					
+					case 'post':
+						
+						$published = $wp->status == 'publish' ? 'yes' : 'no';
 
-				$body = str_replace ("\n", "<br />\n", (string) $content->encoded);
-				$body = preg_replace_callback (
-					'|"https?://[^"]+?/wp-content/uploads/([^"]+)?"|i',
-					'blog_import_wordpress_fix_links',
-					$body
-				);
+						$body = str_replace ("\n", "<br />\n", (string) $content->encoded);
+						$body = preg_replace_callback (
+							'|"https?://[^"]+?/wp-content/uploads/([^"]+)?"|i',
+							'blog_import_wordpress_fix_links',
+							$body
+						);
 
-				$post = array (
-					'title' => (string) $entry->title,
-					'author' => (string) $dc->creator,
-					'ts' => gmdate ('Y-m-d H:i:s', strtotime ($entry->pubDate)),
-					'published' => $published,
-					'body' => $body,
-					'tags' => ''
-				);
-				$sep = '';
-				for ($i = 0; $i < count ($entry->category); $i++) {
-					$post['tags'] .= $sep . $entry->category[$i]->attributes ()->nicename;
-					$sep = ', ';
-				}
-				$p = new blog\Post ($post);
-				if ($p->put ()) {
-					Versions::add ($p);
-
-					// add tags
-					if ($published == 'yes' && ! empty ($post['tags'])) {
-						$tags = explode (',', $post['tags']);
-						foreach ($tags as $tag) {
-							$tr = trim ($tag);
-							DB::execute ('insert into #prefix#blog_tag (id) values (?)', $tr);
-							DB::execute (
-								'insert into #prefix#blog_post_tag (tag_id, post_id) values (?, ?)',
-								$tr,
-								$p->id
-							);
+						$post = array (
+							'title' => (string) $entry->title,
+							'author' => (string) $dc->creator,
+							'ts' => gmdate ('Y-m-d H:i:s', strtotime ($entry->pubDate)),
+							'published' => $published,
+							'body' => $body,
+							'tags' => ''
+						);
+						$sep = '';
+						for ($i = 0; $i < count ($entry->category); $i++) {
+							if ('uncategorized' != ($tag = urldecode ($entry->category[$i]->attributes ()->nicename))) {
+								$post['tags'] .= $sep . $tag;
+							}
+							$sep = ', ';
 						}
-					}
+						$p = new blog\Post ($post);
+						if ($p->put ()) {
+							Versions::add ($p);
 
-					$imported++;
+							// add tags
+							if ($published == 'yes' && ! empty ($post['tags'])) {
+								$tags = explode (',', $post['tags']);
+								foreach ($tags as $tag) {
+									$tr = trim ($tag);
+									DB::execute ('insert into #prefix#blog_tag (id) values (?)', $tr);
+									DB::execute (
+										'insert into #prefix#blog_post_tag (tag_id, post_id) values (?, ?)',
+										$tr,
+										$p->id
+									);
+								}
+							}
+							
+							$links[url_get_fullpath ((string)$entry->link)] = '/blog/post/' . $p->id;
+
+							$imported_posts ++;
+						}						
+						
+						break;
+					
+					case 'page':
+						
+						if ($wp->status == 'publish') {
+							
+							$body = str_replace ("\n", "<br />\n", (string) $content->encoded);
+							$body = preg_replace_callback (
+								'|"https?://[^"]+?/wp-content/uploads/([^"]+)?"|i',
+								'blog_import_wordpress_fix_links',
+								$body
+							);
+
+							$page_data = array (
+								'id' => $wp->post_name,
+								'title' => (string) $entry->title,
+								'body' => $body
+							);
+							
+							$wp = new Webpage ($page_data);
+							$wp->put ();
+							Versions::add ($wp);
+							if ($wp->error) {
+								throw new Exception (__ ('Cannot create page') . ': ' . $wp->error);
+							}
+							
+							$links[url_get_fullpath ((string)$entry->link)] = '/' . $page_data['id'];
+							
+							$imported_pages ++;
+						}
+						
+						break;					
 				}
+				
 			}
 			
-			echo '<p>' . __ ('Imported %d posts.', $imported) . '</p>';
-			echo '<p><a href="/blog/admin">' . __ ('Continue') . '</a></p>';
+			echo $tpl->render ('blog/import/wordpress_done', array (
+				'imported_posts' => $imported_posts,
+				'imported_pages' => $imported_pages,
+				'links' => $links
+			));
 		} catch (Exception $e) {
 			echo '<p><strong>' . __ ('Error importing file') . ': ' . $e->getMessage () . '</strong></p>';
 			echo '<p><a href="/blog/admin">' . __ ('Back') . '</a></p>';
