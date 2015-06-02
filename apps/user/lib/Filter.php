a9aea7090014a09eefe300859f0fd856dcedb023
Johnny Broadway <johnny@johnnybroadway.com>
2015-06-02 15:16:02 -0500

    Added user export for importing contacts into other apps

--- apps/user/lib/Filter.php
+++ apps/user/lib/Filter.php
@@ -9,4 +9,8 @@ class Filter {
 	public static function resource_id ($resource) {
 		return str_replace ('/', '-', $resource);
 	}
+	
+	public static function csv_header ($header) {
+		return ucwords (str_replace ('_', ' ', $header));
+	}
 }
