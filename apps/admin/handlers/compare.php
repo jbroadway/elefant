c61c5499ec3d07b5823a3b2ea9b8edfd4d3bc55f
Johnny Broadway <johnny@johnnybroadway.com>
2015-06-27 12:09:25 -0500

    View current version in Versions link was missing

--- apps/admin/handlers/compare.php
+++ apps/admin/handlers/compare.php
@@ -12,12 +12,25 @@ if (! User::require_admin ()) {
 	$this->redirect ('/admin');
 }
 
+if (! isset ($_GET['current'])) {
+	$this->redirect ('/admin');
+}
+
+if (! in_array ($_GET['current'], array ('yes', 'no'))) {
+	$this->redirect ('/admin');
+}
+
+$is_current = ($_GET['current'] === 'yes') ? true : false;
+$is_deleted = false;
+
 $ver = new Versions ($_GET['id']);
 $old = $ver->restore ();
 $class = $ver->class;
 $cur = new $class ($ver->pkey);
 if ($cur->error) {
 	// deleted item
+	$is_deleted = true;
+
 	foreach (json_decode ($ver->serialized) as $key => $value) {
 		$cur->{$key} = $value;
 	}
@@ -44,5 +57,7 @@ echo $tpl->render ('admin/compare', array (
 	'fields' => $data,
 	'class' => $ver->class,
 	'pkey' => $ver->pkey,
-	'ts' => $ver->ts
+	'ts' => $ver->ts,
+	'is_current' => $is_current,
+	'is_deleted' => $is_deleted
 ));
