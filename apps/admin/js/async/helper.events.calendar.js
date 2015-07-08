1f981b3a1d951ebef410c165130bcfaaf7ec2eef
BronyBorn <minecraftsolutionsinc@gmail.com>
2015-07-08 03:04:21 -0500

    Fixed EOL issue

--- apps/admin/js/async/helper.events.calendar.js
+++ apps/admin/js/async/helper.events.calendar.js
@@ -1,1 +1,11 @@
-﻿jQuery(function($){	s = document.createElement('SCRIPT');	s.innerHTML = "jQuery(function($){if (ec = $('#events-calendar').data('fullCalendar')) ec.options['eventAfterAllRender'] = async.bind;});";	document.getElementsByTagName('body')[0].appendChild(s);	$(document).on("async_success",function (e) {		if (ec = $('#events-calendar').data('fullCalendar')) {			ec.options["eventAfterAllRender"] = async.bind;			ec.render();		}	});}
+﻿jQuery(function($){
+	s = document.createElement('SCRIPT');
+	s.innerHTML = "jQuery(function($){if (ec = $('#events-calendar').data('fullCalendar')) ec.options['eventAfterAllRender'] = async.bind;});";
+	document.getElementsByTagName('body')[0].appendChild(s);
+	$(document).on("async_success",function (e) {
+		if (ec = $('#events-calendar').data('fullCalendar')) {
+			ec.options["eventAfterAllRender"] = async.bind;
+			ec.render();
+		}
+	});
+}
