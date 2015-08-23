09958fe63851fe8e72d6d0e9ffa20cbeb7fcced9
jbroadway <johnny@johnnybroadway.com>
2015-08-23 15:53:07 -0500

    Added admin/util/analytics helper to insert Google Analytics tracking code

---
+++ apps/admin/handlers/util/analytics.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * If a Google Analytics ID is set in the site settings, this will
+ * return the Google Analytics code for your site. You can use it in
+ * your layout templates just before the `</body>` tag like this:
+ *
+ *     {! admin/util/analytics !}
+ */
+
+$analytics_id = Appconf::admin ('Site Settings', 'google_analytics_id');
+if ($analytics_id) {
+	echo $tpl->render (
+		'admin/util/analytics',
+		array (
+			'analytics_id' => $analytics_id
+		)
+	);
+}
