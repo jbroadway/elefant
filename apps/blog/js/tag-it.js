69b72f5bc5f6c28ca9363d84704bbf4788587a4f
jbroadway <johnny@johnnybroadway.com>
2015-08-31 14:06:32 -0500

    Updated blog app for jQuery UI compatibility

--- apps/blog/js/tag-it.js
+++ apps/blog/js/tag-it.js
@@ -90,7 +90,7 @@
                 this.options.singleFieldNode = this.element;
                 this.element.css('display', 'none');
             } else {
-                this.tagList = this.element.find('ul, ol').andSelf().last();
+                this.tagList = this.element.find('ul, ol').addBack().last();
             }
 
             this._tagInput = $('<input type="text">').addClass('ui-widget-content');
