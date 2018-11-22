dec7262064cc0832fc1d438ebf5ed4b2d659fe3c
lux <john@flipsidexr.com>
2018-11-22 17:06:05 -0600

    Added usage info to admin/util/select-buttons

--- apps/admin/handlers/util/select-buttons.php
+++ apps/admin/handlers/util/select-buttons.php
@@ -1,7 +1,24 @@
 <?php
 
 /**
- * Converts HTML select elements into a button group.
+ * Converts HTML select elements into button groups.
+ *
+ * In PHP, load it like this:
+ *
+ *     $this->run ('admin/util/select-buttons');
+ *
+ * Or in your view template, load it like this:
+ *
+ *     {! admin/util/select-buttons !}
+ *
+ * Now you can turn any select box into buttons like this:
+ *
+ *     <select name="some-options" class="buttons">
+ *         <option value="one">One</option>
+ *         <option value="two" selected>Two</option>
+ *     </select>
+ *
+ *     <script> $(function () { $('select.buttons').select_buttons (); }); </script>
  */
 
 $page->add_style ('/apps/admin/css/jquery.select-buttons.css?v=1');
