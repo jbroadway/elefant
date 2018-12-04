788fd5fbb6129285bfaf0d2d5ff63a955492e35f
lux <john@flipsidexr.com>
2018-12-03 22:48:13 -0600

    Reduced width of inputs in admin/modal, busted css cache

--- conf/product.php
+++ conf/product.php
@@ -4,7 +4,7 @@ name = Elefant CMS
 website = "http://www.elefantcms.com/"
 logo_login = "/apps/admin/css/admin/elefant_logo_login.png"
 logo_toolbar = "/apps/admin/css/admin/elefant_logo.png"
-stylesheet = "/apps/admin/css/admin.css?v=4"
+stylesheet = "/apps/admin/css/admin.css?v=5"
 toolbar_stylesheet = "/apps/admin/css/top-bar.css"
 admin_layout = admin
 
