d2566f57b6725c8b6a19c84cb496dc22a59118c5
lux <john@flipsidexr.com>
2018-12-09 10:29:15 -0600

    Upgraded URLify to version 1.1.2-stable

--- lib/vendor/URLify.php
+++ lib/vendor/URLify.php
@@ -3,8 +3,9 @@
 /**
  * A PHP port of URLify.js from the Django project
  * (https://github.com/django/django/blob/master/django/contrib/admin/static/admin/js/urlify.js).
- * Handles symbols from Latin languages, Greek, Turkish, Russian, Ukrainian,
- * Czech, Polish, and Latvian. Symbols it cannot transliterate
+ * Handles symbols from Latin languages, Greek, Turkish, Bulgarian, Russian,
+ * Ukrainian, Czech, Polish, Romanian, Latvian, Lithuanian, Vietnamese, Arabic,
+ * Serbian, Azerbaijani, Kazakh and Slovak. Symbols it cannot transliterate
  * it will simply omit.
  *
  * Usage:
@@ -15,7 +16,8 @@
  *     echo URLify::filter ('Lo siento, no hablo español.');
  *     // "lo-siento-no-hablo-espanol"
  */
-class URLify {
+class URLify
+{
 	public static $maps = array (
 		'de' => array ( /* German */
 			'Ä' => 'Ae', 'Ö' => 'Oe', 'Ü' => 'Ue', 'ä' => 'ae', 'ö' => 'oe', 'ü' => 'ue', 'ß' => 'ss',
@@ -25,11 +27,11 @@ class URLify {
 			'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A','Ă' => 'A', 'Æ' => 'AE', 'Ç' =>
 			'C', 'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ì' => 'I', 'Í' => 'I', 'Î' => 'I',
 			'Ï' => 'I', 'Ð' => 'D', 'Ñ' => 'N', 'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' =>
-			'O', 'Ő' => 'O', 'Ø' => 'O','Ș' => 'S','Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ű' => 'U',
+			'O', 'Ő' => 'O', 'Ø' => 'O', 'Œ' => 'OE' ,'Ș' => 'S','Ț' => 'T', 'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ű' => 'U',
 			'Ý' => 'Y', 'Þ' => 'TH', 'ß' => 'ss', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' =>
 			'a', 'å' => 'a', 'ă' => 'a', 'æ' => 'ae', 'ç' => 'c', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e',
 			'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ð' => 'd', 'ñ' => 'n', 'ò' => 'o', 'ó' =>
-			'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ő' => 'o', 'ø' => 'o', 'ș' => 's', 'ț' => 't', 'ù' => 'u', 'ú' => 'u',
+			'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ő' => 'o', 'ø' => 'o', 'œ' => 'oe', 'ș' => 's', 'ț' => 't', 'ù' => 'u', 'ú' => 'u',
 			'û' => 'u', 'ü' => 'u', 'ű' => 'u', 'ý' => 'y', 'þ' => 'th', 'ÿ' => 'y'
 		),
 		'latin_symbols' => array (
@@ -51,14 +53,26 @@ class URLify {
 			'ş' => 's', 'Ş' => 'S', 'ı' => 'i', 'İ' => 'I', 'ç' => 'c', 'Ç' => 'C', 'ü' => 'u', 'Ü' => 'U',
 			'ö' => 'o', 'Ö' => 'O', 'ğ' => 'g', 'Ğ' => 'G'
 		),
+		'bg' => array( /* Bulgarian */
+			'Щ' => 'Sht', 'Ш' => 'Sh', 'Ч' => 'Ch', 'Ц' => 'C', 'Ю' => 'Yu', 'Я' => 'Ya',
+			'Ж' => 'J',   'А' => 'A',  'Б' => 'B',  'В' => 'V', 'Г' => 'G',  'Д' => 'D',
+			'Е' => 'E',   'З' => 'Z',  'И' => 'I',  'Й' => 'Y', 'К' => 'K',  'Л' => 'L',
+			'М' => 'M',   'Н' => 'N',  'О' => 'O',  'П' => 'P', 'Р' => 'R',  'С' => 'S',
+			'Т' => 'T',   'У' => 'U',  'Ф' => 'F',  'Х' => 'H', 'Ь' => '',   'Ъ' => 'A',
+			'щ' => 'sht', 'ш' => 'sh', 'ч' => 'ch', 'ц' => 'c', 'ю' => 'yu', 'я' => 'ya',
+			'ж' => 'j',   'а' => 'a',  'б' => 'b',  'в' => 'v', 'г' => 'g',  'д' => 'd',
+			'е' => 'e',   'з' => 'z',  'и' => 'i',  'й' => 'y', 'к' => 'k',  'л' => 'l',
+			'м' => 'm',   'н' => 'n',  'о' => 'o',  'п' => 'p', 'р' => 'r',  'с' => 's',
+			'т' => 't',   'у' => 'u',  'ф' => 'f',  'х' => 'h', 'ь' => '',   'ъ' => 'a'
+		),
 		'ru' => array ( /* Russian */
 			'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e', 'ё' => 'yo', 'ж' => 'zh',
-			'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
+			'з' => 'z', 'и' => 'i', 'й' => 'i', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o',
 			'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c',
 			'ч' => 'ch', 'ш' => 'sh', 'щ' => 'sh', 'ъ' => '', 'ы' => 'y', 'ь' => '', 'э' => 'e', 'ю' => 'yu',
 			'я' => 'ya',
 			'А' => 'A', 'Б' => 'B', 'В' => 'V', 'Г' => 'G', 'Д' => 'D', 'Е' => 'E', 'Ё' => 'Yo', 'Ж' => 'Zh',
-			'З' => 'Z', 'И' => 'I', 'Й' => 'J', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
+			'З' => 'Z', 'И' => 'I', 'Й' => 'I', 'К' => 'K', 'Л' => 'L', 'М' => 'M', 'Н' => 'N', 'О' => 'O',
 			'П' => 'P', 'Р' => 'R', 'С' => 'S', 'Т' => 'T', 'У' => 'U', 'Ф' => 'F', 'Х' => 'H', 'Ц' => 'C',
 			'Ч' => 'Ch', 'Ш' => 'Sh', 'Щ' => 'Sh', 'Ъ' => '', 'Ы' => 'Y', 'Ь' => '', 'Э' => 'E', 'Ю' => 'Yu',
 			'Я' => 'Ya',
@@ -67,6 +81,10 @@ class URLify {
 		'uk' => array ( /* Ukrainian */
 			'Є' => 'Ye', 'І' => 'I', 'Ї' => 'Yi', 'Ґ' => 'G', 'є' => 'ye', 'і' => 'i', 'ї' => 'yi', 'ґ' => 'g'
 		),
+        'kk' => array ( /* Kazakh */
+            'Ә' => 'A', 'Ғ' => 'G', 'Қ' => 'Q', 'Ң' => 'N', 'Ө' => 'O', 'Ұ' => 'U', 'Ү' => 'U', 'Һ' => 'H',
+            'ә' => 'a', 'ғ' => 'g', 'қ' => 'q', 'ң' => 'n', 'ө' => 'o', 'ұ' => 'u', 'ү' => 'u', 'һ' => 'h',
+        ),
 		'cs' => array ( /* Czech */
 			'č' => 'c', 'ď' => 'd', 'ě' => 'e', 'ň' => 'n', 'ř' => 'r', 'š' => 's', 'ť' => 't', 'ů' => 'u',
 			'ž' => 'z', 'Č' => 'C', 'Ď' => 'D', 'Ě' => 'E', 'Ň' => 'N', 'Ř' => 'R', 'Š' => 'S', 'Ť' => 'T',
@@ -106,7 +124,13 @@ class URLify {
 			'أ' => 'a', 'ب' => 'b', 'ت' => 't', 'ث' => 'th', 'ج' => 'g', 'ح' => 'h', 'خ' => 'kh', 'د' => 'd',
 			'ذ' => 'th', 'ر' => 'r', 'ز' => 'z', 'س' => 's', 'ش' => 'sh', 'ص' => 's', 'ض' => 'd', 'ط' => 't',
 			'ظ' => 'th', 'ع' => 'aa', 'غ' => 'gh', 'ف' => 'f', 'ق' => 'k', 'ك' => 'k', 'ل' => 'l', 'م' => 'm',
-			'ن' => 'n', 'ه' => 'h', 'و' => 'o', 'ي' => 'y'
+			'ن' => 'n', 'ه' => 'h', 'و' => 'o', 'ي' => 'y',
+			'ا' => 'a', 'إ' => 'a', 'آ' => 'a', 'ؤ' => 'o', 'ئ' => 'y', 'ء' => 'aa',
+			'٠' => '0', '١' => '1', '٢' => '2', '٣' => '3', '٤' => '4', '٥' => '5', '٦' => '6', '٧' => '7', '٨' => '8', '٩' => '9',
+		),
+		'fa' => array ( /* Persian */
+			'گ' => 'g', 'ژ' => 'j', 'پ' => 'p', 'چ' => 'ch', 'ی' => 'y', 'ک' => 'k',
+			'۰' => '0', '۱' => '1', '۲' => '2', '۳' => '3', '۴' => '4', '۵' => '5', '۶' => '6', '۷' => '7', '۸' => '8', '۹' => '9',
 		),
 		'sr' => array ( /* Serbian */
 			'ђ' => 'dj', 'ј' => 'j', 'љ' => 'lj', 'њ' => 'nj', 'ћ' => 'c', 'џ' => 'dz', 'đ' => 'dj',
@@ -115,6 +139,9 @@ class URLify {
 		'az' => array ( /* Azerbaijani */
 			'ç' => 'c', 'ə' => 'e', 'ğ' => 'g', 'ı' => 'i', 'ö' => 'o', 'ş' => 's', 'ü' => 'u',
 			'Ç' => 'C', 'Ə' => 'E', 'Ğ' => 'G', 'İ' => 'I', 'Ö' => 'O', 'Ş' => 'S', 'Ü' => 'U'
+		),
+		'sk' => array ( /* Slovak */
+			'ĺ' => 'l', 'ľ' => 'l', 'ŕ' => 'r'
 		)
 	);
 
@@ -150,8 +177,10 @@ class URLify {
 
 	/**
 	 * Initializes the character map.
+     * @param string $language
 	 */
-	private static function init ($language = "") {
+	private static function init ($language = "")
+    {
 		if (count (self::$map) > 0 && (($language == "") || ($language == self::$language))) {
 			return;
 		}
@@ -180,8 +209,10 @@ class URLify {
 
 	/**
 	 * Add new characters to the list. `$map` should be a hash.
+     * @param array $map
 	 */
-	public static function add_chars ($map) {
+	public static function add_chars ($map)
+    {
 		if (! is_array ($map)) {
 			throw new LogicException ('$map must be an associative array.');
 		}
@@ -193,8 +224,10 @@ class URLify {
 	/**
 	 * Append words to the remove list. Accepts either single words
 	 * or an array of words.
+     * @param mixed $words
 	 */
-	public static function remove_words ($words) {
+	public static function remove_words ($words)
+    {
 		$words = is_array ($words) ? $words : array ($words);
 		self::$remove_list = array_merge (self::$remove_list, $words);
 	}
@@ -203,8 +236,12 @@ class URLify {
 	 * Transliterates characters to their ASCII equivalents.
      * $language specifies a priority for a specific language.
      * The latter is useful if languages have different rules for the same character.
+     * @param string $text
+     * @param string $language
+     * @return string
 	 */
-	public static function downcode ($text, $language = "") {
+	public static function downcode ($text, $language = "")
+    {
 		self::init ($language);
 
 		if (preg_match_all (self::$regex, $text, $matches)) {
@@ -220,27 +257,44 @@ class URLify {
 
 	/**
 	 * Filters a string, e.g., "Petty theft" to "petty-theft"
+	 * @param string $text The text to return filtered
+	 * @param int $length The length (after filtering) of the string to be returned
+	 * @param string $language The transliteration language, passed down to downcode()
+	 * @param bool $file_name Whether there should be and additional filter considering this is a filename
+	 * @param bool $use_remove_list Whether you want to remove specific elements previously set in self::$remove_list
+	 * @param bool $lower_case Whether you want the filter to maintain casing or lowercase everything (default)
+	 * @param bool $treat_underscore_as_space Treat underscore as space, so it will replaced with "-"
+     * @return string
 	 */
-	public static function filter ($text, $length = 60, $language = "", $file_name = false) {
+	public static function filter ($text, $length = 60, $language = "", $file_name = false, $use_remove_list = true, $lower_case = true, $treat_underscore_as_space = true)
+    {
 		$text = self::downcode ($text,$language);
 
-		// remove all these words from the string before urlifying
-		$text = preg_replace ('/\b(' . join ('|', self::$remove_list) . ')\b/i', '', $text);
+		if ($use_remove_list) {
+			// remove all these words from the string before urlifying
+			$text = preg_replace ('/\b(' . join ('|', self::$remove_list) . ')\b/i', '', $text);
+		}
 
 		// if downcode doesn't hit, the char will be stripped here
-		$remove_pattern = ($file_name) ? '/[^-.\w\s]/u' : '/[^-\w\s]/u';
+		$remove_pattern = ($file_name) ? '/[^_\-.\-a-zA-Z0-9\s]/u' : '/[^\s_\-a-zA-Z0-9]/u';
 		$text = preg_replace ($remove_pattern, '', $text); // remove unneeded chars
-		$text = str_replace ('_', ' ', $text);             // treat underscores as spaces
-		$text = preg_replace ('/^\s+|\s+$/', '', $text);   // trim leading/trailing spaces
-		$text = preg_replace ('/[-\s]+/', '-', $text);     // convert spaces to hyphens
-		$text = strtolower ($text);                        // convert to lowercase
+		if ($treat_underscore_as_space) {
+		    	$text = str_replace ('_', ' ', $text);             // treat underscores as spaces
+		}
+		$text = preg_replace ('/^\s+|\s+$/u', '', $text);  // trim leading/trailing spaces
+		$text = preg_replace ('/[-\s]+/u', '-', $text);    // convert spaces to hyphens
+		if ($lower_case) {
+			$text = strtolower ($text);                        // convert to lowercase
+		}
+
 		return trim (substr ($text, 0, $length), '-');     // trim to first $length chars
 	}
 
 	/**
 	 * Alias of `URLify::downcode()`.
 	 */
-	public static function transliterate ($text) {
+	public static function transliterate ($text)
+    {
 		return self::downcode ($text);
 	}
 }
