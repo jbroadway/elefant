f499135faf6d624f39744091ec3898e5b3b4f86d
lux <john@flipsidexr.com>
2020-01-03 10:53:11 -0600

    Upgrade schema tweak

--- apps/blog/conf/upgrade_1.1.5_mysql.sql
+++ apps/blog/conf/upgrade_1.1.5_mysql.sql
@@ -1,3 +1,3 @@
 alter table #prefix#blog_post add column slug char(128) not null default '';
-alter table #prefix#blog_post add column description text not null default '';
-alter table #prefix#blog_post add column keywords text not null default '';
+alter table #prefix#blog_post add column description text not null;
+alter table #prefix#blog_post add column keywords text not null;
