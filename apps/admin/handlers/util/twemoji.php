b7dc03f5e47217dc4c1b1e7b6c33a4d609673be4
lux <john@flipsidexr.com>
2020-02-12 20:47:16 -0600

    Upgraded twemoji to 12.1.5 and added integrity check

--- apps/admin/handlers/util/twemoji.php
+++ apps/admin/handlers/util/twemoji.php
@@ -30,7 +30,7 @@
  * https://github.com/twitter/twemoji
  */
 
-$page->add_script ('//twemoji.maxcdn.com/2/twemoji.min.js?11.0');
+$page->add_script ('https://twemoji.maxcdn.com/v/12.1.5/twemoji.min.js', 'head', '', 'sha384-E4PZh8MWwKQ2W7ANni7xwx6TTuPWtd3F8mDRnaMvJssp5j+gxvP2fTsk1GnFg2gG', 'anonymous');
 $page->add_style ('<style>img.emoji { height: 1em; width: 1em; margin: 0 0.5em 0 0.1em; vertical-align: -0.1em; } </style>');
 
 if (isset ($data['body'])) {
