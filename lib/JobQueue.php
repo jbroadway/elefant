2f9faa236fa65c458bd143864b9aa5b1ce065cdd
lux <john@flipsidexr.com>
2020-09-18 15:00:39 -0500

    Added JobQueue class with pheanstalk backend

---
+++ lib/JobQueue.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Implements a basic job queue for executing background tasks.
+ *
+ * Usage:
+ *
+ *     // Post a task to be done
+ *     JobQueue::enqueue ('tube-name', ['data' => '...']);
+ *
+ *     // Look for tasks to be done
+ *     $worker = JobQueue::worker ();
+ *
+ *     $worker->watch ('tube-name')
+ *     $worker->ignore ('default');
+ *
+ *     while ($job = $worker->reserve ()) {
+ *         $payload = $job->getData ();
+ *         $data = json_decode ($payload);
+ *
+ *         // Do something with $data
+ *
+ *         $worker->delete ($job);
+ *     }
+ */
+class JobQueue {
+	private static $backend = null;
+	private static $service = null;
+	
+	private static function conn () {
+		if (self::$backend != null) {
+			return self::$service;
+		}
+		
+		self::$backend = conf ('JobQueue', 'backend');
+		
+		switch (self::$backend) {
+			case 'beanstalkd':
+				require_once ('lib/JobQueueServices/Beanstalkd.php');
+				self::$service = new BeanstalkdJobQueue ();
+				break;
+			default:
+				break;
+		}
+		
+		return self::$service;
+	}
+	
+	/**
+	 * Post a task to the queue.
+	 */
+	public static function enqueue ($tube, $data) {
+		$conn = self::conn ();
+		
+		if ($conn === null) return false;
+		
+		return $conn->enqueue ($tube, $data);
+	}
+	
+	/**
+	 * Return the job queue service that can be used to watch
+	 * for tasks in the queue.
+	 */
+	public static function worker () {
+		return self::conn ();
+	}
+}
---
+++ lib/JobQueueServices/Beanstalkd.php
@@ -0,0 +1,69 @@
+<?php
+
+use Pheanstalk\Pheanstalk;
+use Pheanstalk\Contract\PheanstalkInterface;
+
+/**
+ * Implements a job queue service using Beanstalkd via Pheanstalk.
+ */
+class BeanstalkdJobQueue {
+	private static $pheanstalk = null;
+	
+	private static function conn () {
+		if (self::$pheanstalk === null) {
+			self::$pheanstalk = Pheanstalk::create (
+				conf ('JobQueue', 'host'),
+				conf ('JobQueue', 'port')
+			);
+		}
+
+		return self::$pheanstalk;
+	}
+	
+	/**
+	 * Post a task to the queue.
+	 */
+	public static function enqueue ($tube, $data) {
+		return self::conn ()->useTube ($tube)->put (
+			json_encode ($data),
+			PheanstalkInterface::DEFAULT_PRIORITY,
+			PheanstalkInterface::DEFAULT_DELAY,
+			PheanstalkInterface::DEFAULT_TTR
+		);
+	}
+	
+	/**
+	 * Watch the specified job queue.
+	 */
+	public function watch ($tube) {
+		return self::conn ()->watch ($tube);
+	}
+
+	/**
+	 * Ignore the specified job queue.
+	 */
+	public function ignore ($tube) {
+		return self::conn ()->ignore ($tube);
+	}
+	
+	/**
+	 * Look for the next job in the queue.
+	 */
+	public function reserve () {
+		return self::conn ()->reserve ();
+	}
+	
+	/**
+	 * Delete a job after it's been completed.
+	 */
+	public function delete ($job) {
+		return self::conn ()->delete ($job);
+	}
+	
+	/**
+	 * Bury a job that couldn't be completed.
+	 */
+	public function bury ($job) {
+		return self::conn ()->bury ($job);
+	}
+}
