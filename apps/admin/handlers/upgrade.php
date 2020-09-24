6c0aa66f8c13c283e5c39b2bb7681d6c4e77a0c5
lux <john@flipsidexr.com>
2020-09-24 08:44:32 -0500

    Fixed upgrades missing schema changes for in-between versions

--- apps/admin/handlers/upgrade.php
+++ apps/admin/handlers/upgrade.php
@@ -12,6 +12,60 @@ if ($this->installed ('elefant', ELEFANT_VERSION) === true) {
 
 $page->title = __ ('Upgrading Elefant');
 
+$db = DB::get_connection (1);
+$dbtype = $db->getAttribute (PDO::ATTR_DRIVER_NAME);
+
+$version = ELEFANT_VERSION;
+$current = $this->installed ('elefant', $version);
+
+// get the base new version and current version for comparison
+$base_version = preg_replace ('/-.*$/', '', $version);
+$base_current = preg_replace ('/-.*$/', '', $current);
+
+// find upgrade scripts to apply
+$files = glob ('apps/admin/conf/update/*_' . $dbtype . '.sql');
+$apply = array ();
+foreach ($files as $k => $file) {
+	if (preg_match ('/^apps\/admin\/conf\/update\/([0-9\.-]+)_' . $dbtype . '\.sql$/', $file, $regs)) {
+		if (version_compare ($regs[1], $base_current, '>') && version_compare ($regs[1], $base_version, '<=')) {
+			$apply[$regs[1]] = $file;
+		}
+	}
+}
+
+if (count ($apply) > 0) {
+	// begin the transaction
+	DB::beginTransaction ();
+
+	// apply the upgrade scripts
+	foreach ($apply as $ver => $file) {
+		// parse the database schema into individual queries
+		$sql = sql_split (file_get_contents ($file));
+
+		// execute each query in turn
+		foreach ($sql as $query) {
+			if (! DB::execute ($query)) {
+				// show error and rollback on failures
+				printf (
+					'<p>%s</p><p class="visible-notice">%s: %s</p>',
+					__ ('Upgrade failed on version %s. Rolling back changes.', $ver),
+					__ ('Error'),
+					DB::error ()
+				);
+				DB::rollback ();
+				return;
+			}
+		}
+
+		// add any custom upgrade logic here
+	}
+
+	// commit the transaction
+	DB::commit ();
+}
+
+// Older updates past this point (can be removed in a future release)
+
 DB::single ('select `extra` from `#prefix#webpage` limit 1');
 if (DB::error ()) { // Add extra column to webpage
 	DB::beginTransaction ();
@@ -25,23 +79,7 @@ if (DB::error ()) { // Add extra column to webpage
 	DB::commit ();
 }
 
-$db = DB::get_connection (1);
-$dbtype = $db->getAttribute (PDO::ATTR_DRIVER_NAME);
-
-if (file_exists ('apps/admin/conf/update/' . ELEFANT_VERSION . '_' . $dbtype . '.sql')) {
-	DB::beginTransaction ();
-	
-	$sqldata = sql_split (file_get_contents ('apps/admin/conf/update/' . ELEFANT_VERSION . '_' . $dbtype . '.sql'));
-	
-	foreach ($sqldata as $sql) {
-		if (! DB::execute ($sql)) {
-			DB::rollback ();
-			printf ('<p>Error: %s</p>', DB::error ());
-			return;
-		}
-	}
-	DB::commit ();
-} elseif (ELEFANT_VERSION === '1.3.10') { // Add extra user fields, social links, and notes
+if (ELEFANT_VERSION === '1.3.10') { // Add extra user fields, social links, and notes
 	DB::beginTransaction ();
 	
 	$sqldata = sql_split (file_get_contents ('apps/admin/conf/update/1.3.10_users_' . $dbtype . '.sql'));
@@ -54,7 +92,9 @@ if (file_exists ('apps/admin/conf/update/' . ELEFANT_VERSION . '_' . $dbtype . '
 		}
 	}
 	DB::commit ();
-} elseif (ELEFANT_VERSION === '1.3.6') { // Fix filemanager_prop primary key
+}
+
+if (ELEFANT_VERSION === '1.3.6') { // Fix filemanager_prop primary key
 	DB::beginTransaction ();
 
 	$db = DB::get_connection (1);
