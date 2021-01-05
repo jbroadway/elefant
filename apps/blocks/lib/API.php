2bd20e8d52b68009779a595b2d2628dcac9c9311
lux <john@flipsidexr.com>
2021-01-05 16:31:28 -0600

    Changes to display and change multi-column blocks/group embeds

--- apps/blocks/lib/API.php
+++ apps/blocks/lib/API.php
@@ -2,6 +2,8 @@
 
 namespace blocks;
 
+use Block;
+
 class API extends \Restful {
 	/**
 	 * Update the sorting order of a block group.
@@ -25,5 +27,32 @@ class API extends \Restful {
 		}
 		
 		return $order->orig ();
+	}
+	
+	/**
+	 * Update the column layout for a row in a block group.
+	 */
+	public function post_update_column_layout () {
+		if (! isset ($_POST['block_id'])) return $this->error ('Missing parameter: block_id');
+		if (! isset ($_POST['column_layout'])) return $this->error ('Missing parameter: column_layout');
+
+		$block = new Block ($_POST['block_id']);
+		
+		if ($block->error) {
+			return $this->error ('Block not found.');
+		}
+		
+		if (! in_array ($_POST['column_layout'], Block::$column_layouts)) {
+			return $this->error ('Invalid column layout');
+		}
+		
+		$block->column_layout = $_POST['column_layout'];
+		
+		if (! $block->put ()) {
+			error_log ($block->error);
+			return $this->error ('Internal server error.');
+		}
+		
+		return true;
 	}
 }
