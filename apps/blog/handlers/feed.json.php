6ec02ebb6bbbdb345355d38e68480e3c87247572
lux <john@flipsidexr.com>
2022-08-08 11:24:33 -0500

    Updated spacing

--- apps/blog/handlers/feed.json.php
+++ apps/blog/handlers/feed.json.php
@@ -10,7 +10,7 @@ if (! $res) {
 	$page->posts = $p->latest (10, 0);
 	$page->title = $appconf['Blog']['title'];
 	$page->date = gmdate ('Y-m-d\TH:i:s');
-	
+
 	$json = [
 		'version' => 'https://jsonfeed.org/version/1',
 		'title' => $page->title,
@@ -34,36 +34,36 @@ if (! $res) {
 			$post->slug = URLify::filter ($post->title);
 			$post->put ();
 		}
-		
+
 		$page->posts[$k]->url = '/blog/post/' . $post->id . '/' . $post->slug;
 		if (Appconf::blog ('Blog', 'post_format') === 'html') {
 			$page->posts[$k]->body = $tpl->run_includes ($page->posts[$k]->body);
 		} else {
 			$page->posts[$k]->body = $tpl->run_includes (Markdown ($page->posts[$k]->body));
 		}
-		
+
 		if ($post->thumbnail !== '') {
 			$page->posts[$k]->image = ($this->is_https () ? 'https' : 'http') . '://'. conf ('General', 'site_domain') . str_replace (' ', '%20', $post->thumbnail);
 		} else {
 			$page->posts[$k]->image = '';
 		}
-		
+
 		// Strip script, iframe, link, and video tags
 		$html = preg_replace ('#<script(.*?)>(.*?)</script>#is', '', $page->posts[$k]->body);
 		$html = preg_replace ('#<style(.*?)>(.*?)</style>#is', '', $page->posts[$k]->body);
 		$html = preg_replace ('#<iframe(.*?)>(.*?)</iframe>#is', '', $html);
 		$html = preg_replace ('#<link(.*?)>#is', '', $html);
 		$html = preg_replace ('#<video(.*?)>(.*?)</video>#is', '', $html);
-		
+
 		// Make sure all URLs are absolutized
 		$html = preg_replace ('/(src|href)="\//i', '\1="' . $this->absolutize ('/'), $html);
-		
+
 		$page->posts[$k]->body = $html;
-		
+
 		if ($preview_chars) {
 			$page->posts[$k]->body = blog_filter_truncate ($page->posts[$k]->body, $preview_chars);
 		}
-		
+
 		$item = [
 			'id' => $page->posts[$k]->id,
 			'title' => $page->posts[$k]->title,
@@ -71,11 +71,11 @@ if (! $res) {
 			'url' => $this->absolutize ($page->posts[$k]->url),
 			'date_published' => str_replace (' ', 'T', $page->posts[$k]->ts) . '+00:00'
 		];
-		
+
 		if ($page->posts[$k]->image != '') {
 			$item['image'] = $page->posts[$k]->image;
 		}
-		
+
 		$json['items'][] = $item;
 	}
 
