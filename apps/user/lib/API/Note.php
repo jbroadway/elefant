b03c24f3020f0986aa8dfbacaf319511fa2fa2c5
lux <john@flipsidexr.com>
2022-08-08 11:22:12 -0500

    Updated spacing

--- apps/user/lib/API/Note.php
+++ apps/user/lib/API/Note.php
@@ -13,7 +13,7 @@ class Note extends Restful {
 	public function post_add () {
 		if (! isset ($_POST['user'])) return $this->error ('Missing parameter: user');
 		if (! isset ($_POST['note'])) return $this->error ('Missing parameter: note');
-		
+
 		$note = new \user\Note (array (
 			'user_id' => $_POST['user'],
 			'ts' => gmdate ('Y-m-d H:i:s'),
@@ -31,13 +31,13 @@ class Note extends Restful {
 	public function post_delete () {
 		if (! isset ($_POST['id'])) return $this->error ('Missing parameter: id');
 		if (! isset ($_POST['user'])) return $this->error ('Missing parameter: user');
-		
+
 		$note = new \user\Note ($_POST['id']);
 		if ($note->error) {
 			error_log ($note->error);
 			return $this->error ('An unexpected error occurred.');
 		}
-		
+
 		if (! $note->remove ()) {
 			error_log ($note->error);
 			return $this->error ('An unexpected error occurred.');
